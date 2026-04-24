5fb2af3eecde2dc3186a9c7d331e2efabf1ea971
a.matinfar <alimatinfar1376@gmail.com>
2026-04-24 18:12:40 +0330

    [TASK]: set validation for CustomerNumberField.tsx

--- src/pages/search/form/formFields/CustomerNumberField/CustomerNumberField.tsx
+++ src/pages/search/form/formFields/CustomerNumberField/CustomerNumberField.tsx
@@ -4,7 +4,9 @@ import useGetFormErrorMessage
   from "../../../../../components/Form/FormLayout/ReactHookFormWrapper/hooks/useGetFormErrorMessage.ts";
 import InputForm from "../../../../../components/Form/Input/InputForm.tsx";
 import {customerNumberFieldLabel, customerNumberFieldName} from "./CustomerNumberField.constances.ts";
+import FORM_PATTERNS from "../../../../../constances/form/formPatterns.ts";
 
+const SPECIFIC_LENGTH = 10
 
 function CustomerNumberField() {
 
@@ -19,9 +21,11 @@ function CustomerNumberField() {
         errorMessage,
         label: customerNumberFieldLabel,
         placeholder: 'برای مثال 12345789',
+        maxLength: SPECIFIC_LENGTH,
       }}
       rules={{
         required: requiredErrorMessage,
+        minLength: FORM_PATTERNS.SPECIFIC_LENGTH_CHAR(SPECIFIC_LENGTH)
       }}
     />
   );
