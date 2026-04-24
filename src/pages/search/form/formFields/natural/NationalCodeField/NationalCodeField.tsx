f26f60ec9f8647c952940510ff40f4f245ef6f10
ali <alimatinfar1376@gmail.com>
2026-04-24 17:43:30 +0330

    [TASK]: set validation for NationalCodeField

--- src/pages/search/form/formFields/natural/NationalCodeField/NationalCodeField.tsx
+++ src/pages/search/form/formFields/natural/NationalCodeField/NationalCodeField.tsx
@@ -3,6 +3,7 @@ import useGetFormErrorMessage
 import InputForm from "../../../../../../components/Form/Input/InputForm.tsx";
 import {nationalCodeFieldLabel, nationalCodeFieldName} from "./NationalCodeField.constances.ts";
 
+const SPECIFIC_LENGTH = 10
 
 function NationalCodeField() {
   const getErrorMessage = useGetFormErrorMessage();
@@ -15,6 +16,10 @@ function NationalCodeField() {
         errorMessage,
         label: nationalCodeFieldLabel,
         placeholder: 'برای مثال 123457890',
+        maxLength: SPECIFIC_LENGTH,
+      }}
+      rules={{
+        minLength: FORM_PATTERNS.MIN_LENGTH_CHAR(SPECIFIC_LENGTH)
       }}
     />
   );
