16c85e74c19a38daf4a978996e870a7365af0534
ali matinfar <a.matinfar@dpkharazmi.com>
2026-04-30 13:38:47 +0330

    [REFACTOR]: refactor SearchPageResultPersonCategory.tsx and move logic to useSearchPageResultPersonCategory.ts

--- npr/DPK-risk/src/pages/search/result/SearchPageResultPersonCategory.tsx
+++ npr/DPK-risk/src/pages/search/result/SearchPageResultPersonCategory.tsx
@@ -1,41 +1,24 @@
 import Button from "../../../components/Form/Button/Button.tsx";
 import {SEARCH_PAGE_FORM_PERSON_TYPE_KEYS} from "../form/SearchPageForm.constances.ts";
 import ArrowIcon from "../../../components/svg/ArrowIcon.tsx";
-import {useMemo, useState} from "react";
 import type {SearchPageResultProps} from "./SearchPageResult.tsx";
 import SearchPageResultCard from "./SearchPageResultCard.tsx";
+import useSearchPageResultPersonCategory from "./hooks/useSearchPageResultPersonCategory.ts";
 
 
-type Props = {
+export type SearchPageResultPersonCategoryProps = {
   personTypeItem: typeof SEARCH_PAGE_FORM_PERSON_TYPE_KEYS[keyof typeof SEARCH_PAGE_FORM_PERSON_TYPE_KEYS]
 } & Pick<SearchPageResultProps, 'resultData'>
 
 function SearchPageResultPersonCategory(
-  {personTypeItem, resultData}: Props
+  {personTypeItem, resultData}: SearchPageResultPersonCategoryProps
 ) {
 
-  const [open, setOpen] = useState(true);
-
-  function toggleOpen() {
-    setOpen(prev => !prev)
-  }
-
-  const [visibleCount, setVisibleCount] = useState(4)
-
-  function loadMoreHandler() {
-    setVisibleCount(prev => prev + 12)
-  }
-
-  const dataList = useMemo(function () {
-    return resultData.filter(resultItem => resultItem.type === personTypeItem.name)
-  }, [resultData, personTypeItem.name])
-
-
-  const visibleItems = useMemo(function () {
-    return dataList.slice(0, visibleCount)
-  }, [dataList, visibleCount])
-
-  const hasLoadMoreButton = visibleCount < dataList.length
+  const {
+    open, toggleOpen, dataList, visibleItems, hasLoadMoreButton, loadMoreHandler
+  } = useSearchPageResultPersonCategory({
+    resultData, personTypeItem
+  })
 
   return (
     <div className='p-4 flex flex-col gap-y-4 rounded-lg bg-[#F2F4F6]'>
