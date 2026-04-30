8e0a62ac5dc378b362a14bc068a66ec13fce0866
ali matinfar <a.matinfar@dpkharazmi.com>
2026-04-30 13:34:24 +0330

    [TASK]: refactor SearchPage.tsx and move logic in useSearchPage.ts

--- npr/DPK-risk/src/pages/search/SearchPage.tsx
+++ npr/DPK-risk/src/pages/search/SearchPage.tsx
@@ -1,39 +1,16 @@
 import SearchPageForm from "./form/SearchPageForm.tsx";
-import SearchPageResult, {type SearchPageResultProps} from "./result/SearchPageResult.tsx";
+import SearchPageResult from "./result/SearchPageResult.tsx";
 import SaderatLogoAndTypo from "../../components/svg/logo/SaderatLogoAndTypo.tsx";
 import MainLayoutProfileSection from "../../layouts/MainLayout/MainLayoutProfileSection.tsx";
 import ZeroAndOneBg from '../../assets/images/zero&one.svg'
-import {useState} from "react";
-import {SEARCH_PAGE_FAKE_DATA} from "./SearchPage.constances.ts";
-import useReactHookFormWrapper
-  from "../../components/Form/FormLayout/ReactHookFormWrapper/hooks/useReactHookFormWrapper.ts";
-import type {SearchPageFormDataType} from "./form/SearchPageForm.types.ts";
-import scrollToElementById from "../../utils/scrollToElementById.ts";
-import {ELEMENT_IDS} from "../../constances/elementIDs.ts";
-import toastPromise from "../../utils/toastPromise.ts";
+import useSearchPage from "./hooks/useSearchPage.ts";
+
 
 function SearchPage() {
 
-  const [resultData, setResultData] = useState<SearchPageResultProps['resultData']>([])
-
-  function onSubmitHandler(formData: SearchPageFormDataType) {
-    if (Object.values(formData).every(value => !value))
-      return toastPromise().then(toast => toast.error('تکمیل حداقل یک فیلد الزامی است'))
-
-    console.log({formData})
-    setResultData(SEARCH_PAGE_FAKE_DATA)
-    setTimeout(() => scrollToElementById(ELEMENT_IDS.SEARCH_RESULT), 300)
-  }
-
   const {
-    formMethods, onSubmit
-  } = useReactHookFormWrapper<SearchPageFormDataType>({
-    onSubmitHandler
-  })
-
-  // const resultData: SearchPageResultProps['resultData'] = useMemo(function () {
-  //   return
-  // }, [])
+    formMethods, onSubmit, resultData
+  } = useSearchPage()
 
   return (
     <>
