d047c1d23a1d1befd6311188a5f0cce0ab7c59ca
ali matinfar <a.matinfar@dpkharazmi.com>
2026-04-30 14:39:12 +0330

    [TASK]: implemented active person in PanelTopBar.tsx

--- npr/DPK-risk/src/pages/panel/layout/PanelTopBar.tsx
+++ npr/DPK-risk/src/pages/panel/layout/PanelTopBar.tsx
@@ -9,11 +9,22 @@ import SearchIcon from "../../../components/svg/SearchIcon.tsx";
 import useGetPageTitleFromSideBar from "./hooks/useGetPageTitleFromSideBar.ts";
 import {Link} from "react-router";
 import ROUTER_LINKS from "../../../constances/routerLinks.ts";
+import useSearchPageResultCard from "../../search/result/hooks/useSearchPageResultCard.ts";
+import getActivePersonData from "../utils/getActivePersonData.ts";
+import getPersonIconByType from "../../search/form/utils/getPersonIconByType.ts";
 
 
 function PanelTopBar() {
 
   const {pageTitle} = useGetPageTitleFromSideBar()
+
+  const activePersonData = getActivePersonData()
+
+  const {
+    fields
+  } = useSearchPageResultCard({
+    data: activePersonData
+  })
 
   return (
     <div className={`
@@ -27,15 +38,39 @@ function PanelTopBar() {
           {pageTitle}
         </span>
 
-        <MainLayoutProfileSection />
+        <MainLayoutProfileSection/>
       </div>
 
-      <div className='flex-1 flex items-center justify-between border-y border-gray-200 bg-[#DDE8F2] px-4'>
-        <span></span>
+      <div className='flex-1 flex items-center justify-between gap-x-4 border-y border-gray-200 bg-[#DDE8F2] px-4'>
+        <div className='flex items-center gap-x-12'>
+          <div className='flex items-center gap-x-2'>
+            {getPersonIconByType(activePersonData.type)({})}
+
+            <span className='text-gray-900 '>
+              {activePersonData?.name}
+            </span>
+          </div>
+
+          {fields.map((field, index) => {
+            return (
+              <div
+                key={index}
+                className='flex items-center gap-x-2 '
+              >
+                <span className='text-xs text-gray-500'>
+                  {field.label}:
+                </span>
+                <span className='text-sm text-gray-900'>
+                  {field.value}
+                </span>
+              </div>
+            )
+          })}
+        </div>
 
         <Link to={ROUTER_LINKS.SEARCH}>
           <Button
-            variant='default' color='white' rightIcon={<SearchIcon textColor='text-gray-500' />}
+            variant='default' color='white' rightIcon={<SearchIcon textColor='text-gray-500'/>}
           >
             ویرایش جستجو
           </Button>
