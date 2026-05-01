70f5f7fb77cbcd86fa662a57474a708893f534a4
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-01 15:55:05 +0330

    [TASK]: finished implementation of PanelEconomicInformationPagePrimarySourceOfIncome.tsx

--- src/pages/panel/economic-information/economic-activity/PanelEconomicInformationPageEconomicActivity.tsx
+++ src/pages/panel/economic-information/economic-activity/PanelEconomicInformationPageEconomicActivity.tsx
@@ -1,15 +1,15 @@
 import Table from "../../../../components/others/Table/Table.tsx";
 import {
-  PANEL_ECONOMIC_INFORMATION_TABLE_COLUMNS,
-  PANEL_ECONOMIC_INFORMATION_TABLE_FAKE_DATA
+  PANEL_ECONOMIC_ACTIVITY_INFORMATION_TABLE_COLUMNS,
+  PANEL_ECONOMIC_ACTIVITY_INFORMATION_TABLE_FAKE_DATA
 } from "./index.contances.ts";
 
 
 function PanelEconomicInformationPageEconomicActivity() {
   return (
     <Table
-      columns={PANEL_ECONOMIC_INFORMATION_TABLE_COLUMNS}
-      data={PANEL_ECONOMIC_INFORMATION_TABLE_FAKE_DATA}
+      columns={PANEL_ECONOMIC_ACTIVITY_INFORMATION_TABLE_COLUMNS}
+      data={PANEL_ECONOMIC_ACTIVITY_INFORMATION_TABLE_FAKE_DATA}
     />
   );
 }
--- src/pages/panel/economic-information/primary-source-of-income/PanelEconomicInformationPagePrimarySourceOfIncome.tsx
+++ src/pages/panel/economic-information/primary-source-of-income/PanelEconomicInformationPagePrimarySourceOfIncome.tsx
@@ -1,8 +1,16 @@
+import Table from "../../../../components/others/Table/Table.tsx";
+import {
+  PANEL_ECONOMIC_PRIMARY_SOURCE_OF_INCOME_INFORMATION_TABLE_COLUMNS,
+  PANEL_ECONOMIC_PRIMARY_SOURCE_OF_INCOME_INFORMATION_TABLE_FAKE_DATA
+} from "./index.constances.ts";
 
 
 function PanelEconomicInformationPagePrimarySourceOfIncome() {
   return (
-    <div></div>
+    <Table
+      columns={PANEL_ECONOMIC_PRIMARY_SOURCE_OF_INCOME_INFORMATION_TABLE_COLUMNS}
+      data={PANEL_ECONOMIC_PRIMARY_SOURCE_OF_INCOME_INFORMATION_TABLE_FAKE_DATA}
+    />
   );
 }
 
