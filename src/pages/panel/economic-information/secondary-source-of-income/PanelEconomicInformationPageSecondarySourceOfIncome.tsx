6679f3399ad1969efd92b12b386873c23ca21037
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-01 16:03:56 +0330

    [TASK]: finished implementation of PanelEconomicInformationPageSecondarySourceOfIncome.tsx

--- src/pages/panel/economic-information/secondary-source-of-income/PanelEconomicInformationPageSecondarySourceOfIncome.tsx
+++ src/pages/panel/economic-information/secondary-source-of-income/PanelEconomicInformationPageSecondarySourceOfIncome.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Table from "../../../../components/others/Table/Table.tsx";
+import {
+  PANEL_ECONOMIC_SECONDARY_SOURCE_OF_INCOME_INFORMATION_TABLE_COLUMNS,
+  PANEL_ECONOMIC_SECONDARY_SOURCE_OF_INCOME_INFORMATION_TABLE_FAKE_DATA
+} from "./index.constances.ts";
 
 function PanelEconomicInformationPageSecondarySourceOfIncome() {
   return (
-    <div></div>
+    <Table
+      columns={PANEL_ECONOMIC_SECONDARY_SOURCE_OF_INCOME_INFORMATION_TABLE_COLUMNS}
+      data={PANEL_ECONOMIC_SECONDARY_SOURCE_OF_INCOME_INFORMATION_TABLE_FAKE_DATA}
+    />
   );
 }
 
