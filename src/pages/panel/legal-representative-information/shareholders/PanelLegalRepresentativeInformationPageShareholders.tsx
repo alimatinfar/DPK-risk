0cd9b8c1c7e51d84aa16facd29e3a08e414340eb
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-03 16:52:26 +0330

    [TASK]: added legal representative shareholders table

--- src/pages/panel/legal-representative-information/shareholders/PanelLegalRepresentativeInformationPageShareholders.tsx
+++ src/pages/panel/legal-representative-information/shareholders/PanelLegalRepresentativeInformationPageShareholders.tsx
@@ -1,8 +1,16 @@
+import Table from "../../../../components/others/Table/Table.tsx";
+import {
+  PANEL_LEGAL_REPRESENTATIVE_SHAREHOLDERS_TABLE_COLUMNS,
+  PANEL_LEGAL_REPRESENTATIVE_SHAREHOLDERS_TABLE_FAKE_DATA
+} from "./index.constances.ts";
 
 
 function PanelLegalRepresentativeInformationPageShareholders() {
   return (
-    <div></div>
+    <Table
+      columns={PANEL_LEGAL_REPRESENTATIVE_SHAREHOLDERS_TABLE_COLUMNS}
+      data={PANEL_LEGAL_REPRESENTATIVE_SHAREHOLDERS_TABLE_FAKE_DATA}
+    />
   );
 }
 
