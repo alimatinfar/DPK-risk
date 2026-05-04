cd70b09c9c198763f6bb40a8a31f6734a304004d
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-04 17:29:11 +0330

    [TASK]: developing on connect legal inspector to api

--- src/pages/panel/legal-representative-information/legalInspector/PanelLegalRepresentativeInformationPageLegalInspector.tsx
+++ src/pages/panel/legal-representative-information/legalInspector/PanelLegalRepresentativeInformationPageLegalInspector.tsx
@@ -1,16 +1,54 @@
 import {
   PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS,
-  PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_FAKE_DATA
+  PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS
 } from "../statutoryAuditor/index.constances.ts";
 import Table from "../../../../components/others/Table/Table.tsx";
+import getActivePersonData from "../../utils/getActivePersonData.ts";
+import useFetchData from "../../../../request/hooks/useFetchData.ts";
+import APIS from "../../../../request/constances/apis.ts";
+import {useMemo} from "react";
+import type {PanelLegalRepresentativeLegalInspectorResponseType} from "./index.types.ts";
+import RenderLogic from "../../../../components/others/RenderLogic/RenderLogic.tsx";
 
 
 function PanelLegalRepresentativeInformationPageLegalInspector() {
+
+  const {getActivePersonNationalId} = getActivePersonData()
+
+  const {
+    data, isFetching, error
+  } = useFetchData<PanelLegalRepresentativeLegalInspectorResponseType>({
+    axiosConfig: {
+      url: APIS.GET_LEGAL_INSPECTOR_INFO,
+      params: {
+        nationalID: getActivePersonNationalId()
+      }
+    }
+  })
+
+  const tableData = useMemo(function () {
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.CUSTOMER_NUMBER]: '50005678',
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.FIRST_NAME]: 'مریم',
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.LAST_NAME]: 'جعفری',
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.NATIONAL_CODE]: '0998877665',
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.FROM_DATE]: '2023-01-01',
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.TO_DATE]: '-',
+    }))
+  }, [data])
+
   return (
-    <Table
-      columns={PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS}
-      data={PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_FAKE_DATA}
-    />
+    <RenderLogic
+      error={error} isLoading={isFetching}
+    >
+      <Table
+        columns={PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS}
+        data={tableData}
+      />
+    </RenderLogic>
   );
 }
 
