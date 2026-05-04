f7507fda3243185c62daeeda8aa8d2847f9f5233
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-04 17:45:45 +0330

    [TASK]: developing on connect legal representative to api

--- src/pages/panel/legal-representative-information/legalRepresentative/PanelLegalRepresentativeInformationPageLegalRepresentative.tsx
+++ src/pages/panel/legal-representative-information/legalRepresentative/PanelLegalRepresentativeInformationPageLegalRepresentative.tsx
@@ -1,16 +1,62 @@
 import Table from "../../../../components/others/Table/Table.tsx";
 import {
   PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS,
-  PANEL_LEGAL_REPRESENTATIVE_TABLE_FAKE_DATA
+  PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS
 } from "./index.constances.ts";
+import getActivePersonData from "../../utils/getActivePersonData.ts";
+import useFetchData from "../../../../request/hooks/useFetchData.ts";
+import APIS from "../../../../request/constances/apis.ts";
+import {useMemo} from "react";
+import type {PanelLegalRepresentativeInformationResponseType} from "./index.types.ts";
+import RenderLogic from "../../../../components/others/RenderLogic/RenderLogic.tsx";
 
 
 function PanelLegalRepresentativeInformationPageLegalRepresentative() {
+
+  const {getActivePersonNationalId} = getActivePersonData()
+
+  const {
+    data, isFetching, error
+  } = useFetchData<PanelLegalRepresentativeInformationResponseType>({
+    axiosConfig: {
+      url: APIS.GET_LEGAL_INSPECTOR_INFO,
+      params: {
+        nationalID: getActivePersonNationalId()
+      }
+    }
+  })
+
+  const tableData = useMemo(function () {
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.CUSTOMER_NUMBER]: '10004567',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.FIRST_NAME]: 'سعید',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.LAST_NAME]: 'رضایی',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.NATIONAL_CODE_OR_FOREIGN_ID]: '0012354782',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.RELATION]: 'وکالت',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.NATIONALITY]: 'ایرانی',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.DOCUMENT_TYPE]: 'وکالت نامه',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.DOCUMENT_NUMBER_OR_CERTIFICATE_NUMBER]: 'V-35670',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.AUTHENTICATION_CODE]: 'XYZ123',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.ISSUING_AUTHORITY]: 'دادگستری تهران',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.PLACE_OF_ISSUE]: 'تهران',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.ISSUE_DATE]: '2025-10-01',
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.EXPIRATION_DATE]: '2027-10-01',
+    }))
+  }, [data])
+
+
   return (
-    <Table
-      columns={PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS}
-      data={PANEL_LEGAL_REPRESENTATIVE_TABLE_FAKE_DATA}
-    />
+    <RenderLogic
+      error={error} isLoading={isFetching}
+    >
+      <Table
+        columns={PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS}
+        data={tableData}
+      />
+    </RenderLogic>
   );
 }
 
--- src/pages/panel/legal-representative-information/legalRepresentative/index.types.ts
+++ src/pages/panel/legal-representative-information/legalRepresentative/index.types.ts
@@ -1,11 +1,3 @@
 export type PanelLegalRepresentativeInformationResponseType = {
-  nationalID: number;
-  ratio: string;
-  certificateType: number;
-  certificateNumber: number;
-  authenticationCode: number;
-  issuingAuthority: string;
-  dateOfIssuance: number;
-  expirationDate: number;
-  issuancePlace: string;
-}
+
+}[]
