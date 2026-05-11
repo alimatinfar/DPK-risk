6fd30914b2c18087f3dcc4753b81771a999bc6bd
k.behrouzi <k.behrouzi@dpkharazmi.com>
2026-05-11 16:40:04 +0330

    history tag and button in page

---
+++ src/pages/panel/comprehensive-info/basic-information/PanelBasicInformationPageHistory.tsx
@@ -0,0 +1,48 @@
+import Button from "../../../../components/Form/Button/Button";
+import Tag from "../../../../components/others/Tag/Tag";
+import CalendarIcon from "../../../../components/svg/CalendarIcon";
+import ClockBackwardIcon from "../../../../components/svg/ClockBackwardIcon";
+import VectorIcon from "../../../../components/svg/VectorIcon";
+import type { HistoryDataType } from "./index.constances";
+
+export type PanelBasicInformationPageHistoryProps = {
+  openHistoryModalHandler: () => void;
+  activeHistory: HistoryDataType;
+};
+
+function PanelBasicInformationPageHistory(
+    {openHistoryModalHandler, activeHistory}: PanelBasicInformationPageHistoryProps
+) {
+    return (
+        <>
+            {
+                activeHistory &&
+                <div className="flex-1 flex gap-2">
+                    <Tag
+                        text={activeHistory.date}
+                        variant="fade"
+                        color="cyan"
+                        rightIcon={<CalendarIcon />}
+                    />
+                    <Tag
+                        text={activeHistory.location}
+                        variant="fade"
+                        color="blue"
+                        rightIcon={<VectorIcon />}
+                    />
+                </div>
+            }
+            <Button
+                variant="link"
+                color="secondary"
+                size="sm"
+                rightIcon={<ClockBackwardIcon width={15} height={15}/>}
+                onClick={openHistoryModalHandler}
+            >
+                مشاهده تاریخچه
+            </Button>
+        </>
+    );
+}
+
+export default PanelBasicInformationPageHistory;
