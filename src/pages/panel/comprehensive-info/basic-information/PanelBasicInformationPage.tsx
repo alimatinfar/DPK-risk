6a6eeca8de0657c87ce8845c2580f41eef2a0417
k.behrouzi <k.behrouzi@dpkharazmi.com>
2026-05-12 07:30:49 +0330

    add history to panel basic page

--- src/pages/panel/comprehensive-info/basic-information/PanelBasicInformationPage.tsx
+++ src/pages/panel/comprehensive-info/basic-information/PanelBasicInformationPage.tsx
@@ -1,38 +1,73 @@
 import PanelPageTitle from "../../layout/PanelPageTitle.tsx";
-import {DetailInfoSection} from "../../../../components/others/DetailInfoSection";
+import { DetailInfoSection } from "../../../../components/others/DetailInfoSection";
 import usePanelBasicInformationPage from "./hooks/usePanelBasicInformationPage.ts";
 import RenderLogic from "../../../../components/others/RenderLogic/RenderLogic.tsx";
+import PanelBasicInformationPageHistory from "./PanelBasicInformationPageHistory.tsx";
+import useModalOpenBoolean from "../../../../hooks/modal/useModalOpenBoolean.ts";
+import DisplayModal from "../../../../components/others/Modal/DisplayModal.tsx";
+import PanelBasicInfoHistoryModal from "./PanelBasicInfoHistoryModal.tsx";
+import useSelectActiveHistory from "./hooks/useSelectActiveHistory.ts";
 
 function PanelBasicInformationPage() {
 
+  const {activeHistory, setActiveHistory} = useSelectActiveHistory();
+
   const {
-    identityInfoList, homeAddressList, officeAddressList, workplaceAddressList, isLegalBool,
+    identityInfoList, homeAddressList, officeAddressList, workplaceAddressList, isLegalBool, infoHistoryList,
     loading, error
   } = usePanelBasicInformationPage()
 
+  const {
+    openModalHandler: openHistoryModalHandler,
+    open: historyModalOpen,
+    closeModalHandler: closeHistoryModalHandler
+  } = useModalOpenBoolean(false)
+
   return (
-    <div className='flex flex-col gap-y-4'>
-      <PanelPageTitle/>
-
-      <RenderLogic
-        isLoading={loading} error={error}
-      >
-        <DetailInfoSection
-          title='اطلاعات هویتی'
-          infoList={identityInfoList}
+    <>
+      <div className='flex flex-col gap-y-4'>
+        <PanelPageTitle
+          children={
+            <RenderLogic
+              isLoading={loading}
+            >
+              <PanelBasicInformationPageHistory
+                openHistoryModalHandler={openHistoryModalHandler}
+                activeHistory={activeHistory}
+              />
+            </RenderLogic>
+          }
         />
 
-        <DetailInfoSection
-          title={isLegalBool ? 'اطلاعات دفتر مرکزی' : 'اطلاعات محل سکونت'}
-          infoList={isLegalBool ? officeAddressList : homeAddressList}
-        />
+        <RenderLogic
+          isLoading={loading} error={error}
+        >
+          <DetailInfoSection
+            title='اطلاعات هویتی'
+            infoList={identityInfoList}
+          />
 
-        <DetailInfoSection
-          title={isLegalBool ? 'اطلاعات محل فعالیت' : 'اطلاعات محل کار'}
-          infoList={workplaceAddressList}
+          <DetailInfoSection
+            title={isLegalBool ? 'اطلاعات دفتر مرکزی' : 'اطلاعات محل سکونت'}
+            infoList={isLegalBool ? officeAddressList : homeAddressList}
+          />
+
+          <DetailInfoSection
+            title={isLegalBool ? 'اطلاعات محل فعالیت' : 'اطلاعات محل کار'}
+            infoList={workplaceAddressList}
+          />
+        </RenderLogic>
+      </div>
+      <DisplayModal>
+        <PanelBasicInfoHistoryModal
+          onClose={closeHistoryModalHandler}
+          open={historyModalOpen}
+          infoHistoryList={infoHistoryList}
+          setHistoryItemClick={setActiveHistory}
+          activeItemId={activeHistory ? activeHistory.id : "1"}
         />
-      </RenderLogic>
-    </div>
+      </DisplayModal>
+    </>
   );
 }
 
