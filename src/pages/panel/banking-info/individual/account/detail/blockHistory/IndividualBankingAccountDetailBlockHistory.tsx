ed661813648194e4d216e93bd5f001c5f535af8c
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-12 21:28:01 +0330

    [TASK]: added table of IndividualBankingAccountDetailBlockHistory.tsx

--- src/pages/panel/banking-info/individual/account/detail/blockHistory/IndividualBankingAccountDetailBlockHistory.tsx
+++ src/pages/panel/banking-info/individual/account/detail/blockHistory/IndividualBankingAccountDetailBlockHistory.tsx
@@ -1,8 +1,16 @@
+import Table from "../../../../../../../components/others/Table/Table.tsx";
+import {
+  INDIVIDUAL_ACCOUNT_BLOCK_HISTORY_TABLE_COLUMNS,
+  INDIVIDUAL_ACCOUNT_BLOCK_HISTORY_TABLE_FAKE_DATA
+} from "./index.constances.ts";
 
 
 function IndividualBankingAccountDetailBlockHistory() {
   return (
-    <div></div>
+    <Table
+      columns={INDIVIDUAL_ACCOUNT_BLOCK_HISTORY_TABLE_COLUMNS}
+      data={INDIVIDUAL_ACCOUNT_BLOCK_HISTORY_TABLE_FAKE_DATA}
+    />
   );
 }
 
