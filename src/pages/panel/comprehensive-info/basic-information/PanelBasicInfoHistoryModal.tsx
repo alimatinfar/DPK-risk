09e633e15c008e4dec7f087316d560330bb593ce
k.behrouzi <k.behrouzi@dpkharazmi.com>
2026-05-13 09:00:10 +0330

    fix setState type error and handle history list empty show in ui

--- src/pages/panel/comprehensive-info/basic-information/PanelBasicInfoHistoryModal.tsx
+++ src/pages/panel/comprehensive-info/basic-information/PanelBasicInfoHistoryModal.tsx
@@ -1,12 +1,13 @@
 import type {ModalProps} from "../../../../components/others/Modal/Modal.tsx";
 import Modal from "../../../../components/others/Modal/Modal.tsx";
+import RenderLogic from "../../../../components/others/RenderLogic/RenderLogic.tsx";
 import type {SetStateType} from "../../../../types/SetStateType.ts";
 import HistoryItemCard from "./HistoryItemCard.tsx";
 import type {HistoryDataType} from "./index.constances.ts";
 
 export type PanelBasicInfoHistoryModalProps = ModalProps & {
   infoHistoryList: HistoryDataType[]
-  setHistoryItemClick: SetStateType<HistoryDataType>
+  setHistoryItemClick: SetStateType<HistoryDataType | undefined>
   activeItemId: string
 }
 
@@ -28,23 +29,27 @@ function PanelBasicInfoHistoryModal(
       {...props}
     >
       <p className="text-base text-secondary-text">با انتخاب هر نسخه، اطلاعات در همان مقطع زمانی نمایش داده می‌شود.</p>
-      <div className="flex flex-col mt-6 gap-2">
-        {
-          infoHistoryList.map(history => (
-            <HistoryItemCard
-              key={history.id}
-              activeId={activeItemId}
-              onItemClick={onItemClick}
-              item={{
-                id: history.id,
-                date: history.date,
-                isLasted: history.isLasted,
-                location: history.location
-              }}
-            />
-          ))
-        }
-      </div>
+      <RenderLogic
+        isEmpty={infoHistoryList.length === 0}
+      >
+        <div className="flex flex-col mt-6 gap-2">
+          {
+            infoHistoryList.map(history => (
+              <HistoryItemCard
+                key={history.id}
+                activeId={activeItemId}
+                onItemClick={onItemClick}
+                item={{
+                  id: history.id,
+                  date: history.date,
+                  isLasted: history.isLasted,
+                  location: history.location
+                }}
+              />
+            ))
+          }
+        </div>
+      </RenderLogic>
     </Modal>
   );
 }
