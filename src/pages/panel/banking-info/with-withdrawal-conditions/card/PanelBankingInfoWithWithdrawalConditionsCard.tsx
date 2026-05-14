b5bb604c2cfa0722584598e74ea3db0ca3c797b8
a.matinfar <alimatinfar1376@gmail.com>
2026-05-14 16:45:42 +0330

    [TASK]: added tableData of PanelBankingInfoWithWithdrawalConditionsCard.tsx

--- src/pages/panel/banking-info/with-withdrawal-conditions/card/PanelBankingInfoWithWithdrawalConditionsCard.tsx
+++ src/pages/panel/banking-info/with-withdrawal-conditions/card/PanelBankingInfoWithWithdrawalConditionsCard.tsx
@@ -1,6 +1,15 @@
+import Table from "../../../../../components/others/Table/Table.tsx";
+import {
+  PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS,
+  PANEL_INDIVIDUAL_CARD_INFO_TABLE_FAKE_DATA
+} from "../../individual/card/index.constances.tsx";
+
 function PanelBankingInfoWithWithdrawalConditionsCard() {
   return (
-    <div></div>
+    <Table
+      columns={PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS}
+      data={PANEL_INDIVIDUAL_CARD_INFO_TABLE_FAKE_DATA}
+    />
   );
 }
 
