7662baf50aaf78d9496f22af97262db7c798b95c
a.matinfar <alimatinfar1376@gmail.com>
2026-05-14 11:02:45 +0330

    [TASK]: added table of IndividualBankingCommitmentsDetailGuarantors.tsx

--- src/pages/panel/banking-info/individual/commitments/detail/beneficiaries/IndividualBankingCommitmentsDetailBeneficiaries.tsx
+++ src/pages/panel/banking-info/individual/commitments/detail/beneficiaries/IndividualBankingCommitmentsDetailBeneficiaries.tsx
@@ -1,14 +1,15 @@
 import Table from "../../../../../../../components/others/Table/Table.tsx";
 import {
-  PANEL_INDIVIDUAL_COMMITMENT_BENEFICIARIES_TABLE_COLUMNS,
-  PANEL_INDIVIDUAL_COMMITMENT_BENEFICIARIES_TABLE_FAKE_DATA
-} from "./index.constances.ts";
+  PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS,
+  PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_FAKE_DATA
+} from "../index.constances.ts";
+
 
 function IndividualBankingCommitmentsDetailBeneficiaries() {
   return (
     <Table
-      columns={PANEL_INDIVIDUAL_COMMITMENT_BENEFICIARIES_TABLE_COLUMNS}
-      data={PANEL_INDIVIDUAL_COMMITMENT_BENEFICIARIES_TABLE_FAKE_DATA}
+      columns={PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS}
+      data={PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_FAKE_DATA}
     />
   )
 }
--- src/pages/panel/banking-info/individual/commitments/detail/guarantors/IndividualBankingCommitmentsDetailGuarantors.tsx
+++ src/pages/panel/banking-info/individual/commitments/detail/guarantors/IndividualBankingCommitmentsDetailGuarantors.tsx
@@ -1,6 +1,15 @@
+import {
+  PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS,
+  PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_FAKE_DATA
+} from "../index.constances.ts";
+import Table from "../../../../../../../components/others/Table/Table.tsx";
+
 function IndividualBankingCommitmentsDetailGuarantors() {
   return (
-    <div></div>
+    <Table
+      columns={PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS}
+      data={PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_FAKE_DATA}
+    />
   );
 }
 
