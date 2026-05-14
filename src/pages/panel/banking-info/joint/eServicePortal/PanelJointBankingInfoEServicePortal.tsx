a2669820e0ad2d6adfed58d278bdb341b1525afb
a.matinfar <alimatinfar1376@gmail.com>
2026-05-14 17:38:05 +0330

    [TASK]: added tableData of PanelJointBankingInfoEServicePortal.tsx

--- src/pages/panel/banking-info/joint/eServicePortal/PanelJointBankingInfoEServicePortal.tsx
+++ src/pages/panel/banking-info/joint/eServicePortal/PanelJointBankingInfoEServicePortal.tsx
@@ -1,6 +1,15 @@
+import Table from "../../../../../components/others/Table/Table.tsx";
+import {
+  PANEL_JOINT_E_SERVICE_PORTAL_TABLE_COLUMNS,
+  PANEL_JOINT_E_SERVICE_PORTAL_TABLE_FAKE_DATA
+} from "./index.constances.tsx";
+
 function PanelJointBankingInfoEServicePortal() {
   return (
-    <div></div>
+    <Table
+      columns={PANEL_JOINT_E_SERVICE_PORTAL_TABLE_COLUMNS}
+      data={PANEL_JOINT_E_SERVICE_PORTAL_TABLE_FAKE_DATA}
+    />
   );
 }
 
