e7c8ac0d8d829bd0b709c9e4359ac76b16e849a2
a.matinfar <alimatinfar1376@gmail.com>
2026-05-14 10:49:28 +0330

    [TASK]: added table of IndividualBankingCommitmentsDetailBeneficiaries.tsx

--- src/pages/panel/banking-info/individual/commitments/detail/beneficiaries/IndividualBankingCommitmentsDetailBeneficiaries.tsx
+++ src/pages/panel/banking-info/individual/commitments/detail/beneficiaries/IndividualBankingCommitmentsDetailBeneficiaries.tsx
@@ -1,6 +1,15 @@
+import Table from "../../../../../../../components/others/Table/Table.tsx";
+import {
+  PANEL_INDIVIDUAL_COMMITMENT_BENEFICIARIES_TABLE_COLUMNS,
+  PANEL_INDIVIDUAL_COMMITMENT_BENEFICIARIES_TABLE_FAKE_DATA
+} from "./index.constances.ts";
+
 function IndividualBankingCommitmentsDetailBeneficiaries() {
   return (
-    <div></div>
+    <Table
+      columns={PANEL_INDIVIDUAL_COMMITMENT_BENEFICIARIES_TABLE_COLUMNS}
+      data={PANEL_INDIVIDUAL_COMMITMENT_BENEFICIARIES_TABLE_FAKE_DATA}
+    />
   )
 }
 
