926a03ae1b1583dd840e53d03717efca895c76b0
a.matinfar <alimatinfar1376@gmail.com>
2026-05-14 17:34:52 +0330

    [TASK]: added tableData of PanelJointBankingInfoCommitments.tsx

--- src/pages/panel/banking-info/joint/commitments/PanelJointBankingInfoCommitments.tsx
+++ src/pages/panel/banking-info/joint/commitments/PanelJointBankingInfoCommitments.tsx
@@ -1,6 +1,33 @@
+import Table from "../../../../../components/others/Table/Table.tsx";
+import {PANEL_JOINT_COMMITMENTS_TABLE_COLUMNS, PANEL_JOINT_COMMITMENTS_TABLE_FAKE_DATA} from "./index.constances.tsx";
+import {useNavigate} from "react-router";
+import {useMemo} from "react";
+import TABLE_ACCESSORS from "../../../../../components/others/Table/constances/tableAccessors.ts";
+import ROUTER_LINKS from "../../../../../constances/routerLinks.ts";
+import ArrowIcon2 from "../../../../../components/svg/ArrowIcon2.tsx";
+
 function PanelJointBankingInfoCommitments() {
+
+  const navigate = useNavigate()
+
+  const tableData = useMemo(function () {
+    return PANEL_JOINT_COMMITMENTS_TABLE_FAKE_DATA.map(item => ({
+      ...item,
+      [TABLE_ACCESSORS.TD_ACTIONS_ACCESSOR]: [
+        {
+          onClick: () => navigate(ROUTER_LINKS.PANEL_JOINT_BANKING_INFORMATION_COMMITMENTS_DETAIL(item.id)),
+          icon: <ArrowIcon2 width='100%' height='100%' />,
+          title: 'جزئیات',
+        }
+      ],
+    }))
+  }, [])
+
   return (
-    <div></div>
+    <Table
+      columns={PANEL_JOINT_COMMITMENTS_TABLE_COLUMNS}
+      data={tableData}
+    />
   );
 }
 
