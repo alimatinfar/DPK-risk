4c1b8d4b619a8554011b301712ac6cf123e02da8
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-18 19:04:21 +0330

    [TASK]: changed sso config logic

--- src/pages/sso-login/SSOLoginPage.tsx
+++ src/pages/sso-login/SSOLoginPage.tsx
@@ -21,10 +21,13 @@ function SSOLoginPage() {
     const config = await loadConfigFile();
     setLoading(true)
     const params = new URLSearchParams({
-      client_id: config.client_id,
+      // client_id: config.client_id,
+      client_id: '87fdee9bdef647aa9c4cf820650fd2db',
       response_type: "id_token",
-      scope: config.scope,
-      redirect_uri: `${import.meta.env.DEV ? 'http://localhost:3030/' : config.frontBaseUrl}auth/sso-login`,
+      // scope: config.scope,
+      scope: 'openid profile',
+      // redirect_uri: `${import.meta.env.DEV ? 'http://localhost:3030/' : config.frontBaseUrl}auth/sso-login`,
+      redirect_uri: `${import.meta.env.DEV ? 'http://localhost:3030/' : 'http://172.16.20.35:8284/'}auth/sso-login`,
       nonce: generateUUID(),
       state: generateUUID()
     });
