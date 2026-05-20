9f8d24aa3fe66e2dfa04237cb150a77bddbdbbcb
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-20 22:42:45 +0330

    [TASK]: connected PanelIndividualBankingInfoCommitments.tsx to api

--- src/pages/panel/banking-info/individual/commitments/PanelIndividualBankingInfoCommitments.tsx
+++ src/pages/panel/banking-info/individual/commitments/PanelIndividualBankingInfoCommitments.tsx
@@ -1,31 +1,70 @@
 import Table from "../../../../../components/others/Table/Table.tsx";
 import {
-  PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS,
-  PANEL_INDIVIDUAL_COMMITMENTS_TABLE_FAKE_DATA
+  PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS, PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS,
 } from "./index.constances.tsx";
 import {useNavigate} from "react-router";
 import {useMemo} from "react";
 import TABLE_ACCESSORS from "../../../../../components/others/Table/constances/tableAccessors.ts";
 import ROUTER_LINKS from "../../../../../constances/routerLinks.ts";
 import ArrowIcon2 from "../../../../../components/svg/ArrowIcon2.tsx";
+import getActivePersonData from "../../../utils/getActivePersonData.ts";
+import useFetchData from "../../../../../request/hooks/useFetchData.ts";
+import APIS from "../../../../../request/constances/apis.ts";
+import type {PanelIndividualBankingInfoCommitmentsResponseType} from "./index.types.ts";
+import withSeparator from "../../../../../utils/separator/withSeparator.ts";
+import displayDate from "../../../../../utils/dateAndTIme/displayDate.ts";
 
 
 function PanelIndividualBankingInfoCommitments() {
 
   const navigate = useNavigate()
 
+
+  const {activePersonData} = getActivePersonData();
+
+  const {
+    data, isFetching, error
+  } = useFetchData<PanelIndividualBankingInfoCommitmentsResponseType>({
+    axiosConfig: {
+      url: APIS.BANK_INFO_GET_CUSTOMER_OBLIGATIONS,
+      method: "POST",
+      params: {
+        customerId: activePersonData?.customerId
+      }
+    }
+  })
+
   const tableData = useMemo(function () {
-    return PANEL_INDIVIDUAL_COMMITMENTS_TABLE_FAKE_DATA.map(item => ({
-      ...item,
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.INDIRECT_COMMITMENT]: item.indirectObligations,
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.COMMITMENT_NUMBER]: item.obligationsNo,
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.PRINCIPAL_AMOUNT]: withSeparator(item.obligationsAmount),
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.TOTAL_AMOUNT]: withSeparator(item.obligationsTotalAmount),
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.INTEREST_RATE]: `${item.interestRate}%`,
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.COMMITMENT_TYPE]: item.obligationsType,
+      // TODO display tag instead only value
+      // [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.STATUS]: (
+      //   <Tag text='فعال' color='green' variant='fade'/>
+      // ),
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.STATUS]: item.status,
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.CENTRAL_BANK_CONTRACT_NUMBER]: item.centralBankNumber,
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.ALLOCATION_DATE]: displayDate(item.allocationDate),
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.END_DATE]: displayDate(item.endDate),
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.BRANCH_CODE]: item.branchCode,
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.BRANCH_NAME]: item.branchName,
+      [PANEL_INDIVIDUAL_COMMITMENTS_TABLE_COLUMNS_KEYS.REGION_NAME]: item.areaName,
       [TABLE_ACCESSORS.TD_ACTIONS_ACCESSOR]: [
         {
-          onClick: () => navigate(ROUTER_LINKS.PANEL_INDIVIDUAL_BANKING_INFORMATION_COMMITMENTS_DETAIL(item.id)),
+          onClick: () => navigate(ROUTER_LINKS.PANEL_INDIVIDUAL_BANKING_INFORMATION_COMMITMENTS_DETAIL(item.obligationsNo)),
           icon: <ArrowIcon2 width='100%' height='100%' />,
           title: 'جزئیات',
         }
       ],
     }))
-  }, [])
+  }, [data])
 
   return (
     <Table
---
+++ src/pages/panel/banking-info/individual/commitments/index.types.ts
@@ -0,0 +1,17 @@
+
+export type PanelIndividualBankingInfoCommitmentsResponseType = {
+  obligationsNo: number;
+  indirectObligations: number;
+  obligationsAmount: number;
+  obligationsTotalAmount: number;
+  interestRate: number;
+  obligationsType: number;
+  status: number;
+  centralBankNumber: number;
+  allocationDate: number;
+  endDate: number;
+  branchCode: number;
+  branchName: string;
+  areaCode: string;
+  areaName: string;
+}[]
