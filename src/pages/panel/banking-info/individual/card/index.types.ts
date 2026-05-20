c9598065b20abbaba0714a55df651048eb0df4ce
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-20 22:17:13 +0330

    [TASK]: connected PanelIndividualBankingInfoCard.tsx to api

---
+++ src/pages/panel/banking-info/individual/card/index.types.ts
@@ -0,0 +1,15 @@
+
+export type PanelIndividualBankingInfoCardResponseType = {
+  cardNumber: number;
+  cardAccount: number;
+  cardState: number;
+  cardType: number;
+  issueDate: number;
+  expirDate: number;
+  branchCode: number;
+  branchName: string;
+  areaCode: string;
+  areaName: string;
+  bankPersonnelCode: number;
+  bankPersonnelName: string;
+}[]
