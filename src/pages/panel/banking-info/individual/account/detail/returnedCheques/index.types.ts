421c3d5026c893321894d650123974ca83aeee7b
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-21 12:38:12 +0330

    [TASK]: added table of IndividualBankingAccountDetailReturnedCheques.tsx

---
+++ src/pages/panel/banking-info/individual/account/detail/returnedCheques/index.types.ts
@@ -0,0 +1,9 @@
+
+export type IndividualBankingAccountDetailReturnedChequesResponseType = {
+  accountNumber: number;
+  cheqNumber: number;
+  amount: number;
+  doneDate: number;
+  returnDate: number;
+  effectDate: number;
+}[]
