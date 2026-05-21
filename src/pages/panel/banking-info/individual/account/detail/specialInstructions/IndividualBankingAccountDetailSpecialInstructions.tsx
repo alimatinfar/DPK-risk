a2cbc65d0a2649ef3b689be8f7ad7577f412f932
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-21 12:50:10 +0330

    [TASK]: added table of IndividualBankingAccountDetailSpecialInstructions.tsx

--- src/pages/panel/banking-info/individual/account/detail/specialInstructions/IndividualBankingAccountDetailSpecialInstructions.tsx
+++ src/pages/panel/banking-info/individual/account/detail/specialInstructions/IndividualBankingAccountDetailSpecialInstructions.tsx
@@ -1,15 +1,54 @@
 import Table from "../../../../../../../components/others/Table/Table.tsx";
 import {
-  INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS,
-  INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_FAKE_DATA
+  INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS, INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS_KEYS,
 } from "./index.constances.ts";
+import {useParams} from "react-router";
+import useFetchData from "../../../../../../../request/hooks/useFetchData.ts";
+import APIS from "../../../../../../../request/constances/apis.ts";
+import {useMemo} from "react";
+import RenderLogic from "../../../../../../../components/others/RenderLogic/RenderLogic.tsx";
+import type {IndividualBankingAccountDetailSpecialInstructionsResponseType} from "./index.types.ts";
+import displayDate from "../../../../../../../utils/dateAndTIme/displayDate.ts";
+
 
 function IndividualBankingAccountDetailSpecialInstructions() {
+
+  const {id: accountNumber} = useParams()
+
+  const {
+    data, isFetching, error
+  } = useFetchData<IndividualBankingAccountDetailSpecialInstructionsResponseType>({
+    axiosConfig: {
+      url: APIS.BANK_INFO_GET_ACCOUNT_SPECIAL_INSTRUCTIONS,
+      method: "POST",
+      params: {
+        accountNumber
+      }
+    }
+  })
+
+  const tableData = useMemo(function () {
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS_KEYS.APPLY_DATE]: displayDate(item.doneDate),
+      [INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS_KEYS.REMOVE_DATE]: displayDate(item.deleteDate),
+      [INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS_KEYS.REASON_CODE]: item.reasonCode,
+      [INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS_KEYS.REASON_DESCRIPTION]: item.reasonDescription,
+    }))
+  }, [data])
+
   return (
-    <Table
-      columns={INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS}
-      data={INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_FAKE_DATA}
-    />
+    <RenderLogic
+      isLoading={isFetching} error={error}
+      isEmpty={tableData?.length === 0}
+    >
+      <Table
+        columns={INDIVIDUAL_ACCOUNT_SPECIAL_INSTRUCTIONS_TABLE_COLUMNS}
+        data={tableData}
+      />
+    </RenderLogic>
   );
 }
 
---
+++ src/pages/panel/banking-info/individual/account/detail/specialInstructions/index.types.ts
@@ -0,0 +1,9 @@
+
+export type IndividualBankingAccountDetailSpecialInstructionsResponseType = {
+  id: number;
+  accountNumber: number;
+  doneDate: number;
+  deleteDate: number;
+  reasonCode: string;
+  reasonDescription: string;
+}[]
