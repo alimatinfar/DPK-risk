25e3ab5a010193f2c7c085cba658a682f072cfa9
ali matinfar <a.matinfar@dpkharazmi.com>
2026-05-21 16:50:16 +0330

    [TASK]: IndividualBankingAccountDetailAccountSignatories.tsx connected to api

--- src/pages/panel/banking-info/individual/account/detail/accountSignatories/IndividualBankingAccountDetailAccountSignatories.tsx
+++ src/pages/panel/banking-info/individual/account/detail/accountSignatories/IndividualBankingAccountDetailAccountSignatories.tsx
@@ -1,13 +1,55 @@
 import Table from "../../../../../../../components/others/Table/Table.tsx";
 import {PANEL_INDIVIDUAL_ACCOUNT_DETAIL_SIGNATORIES_TABLE_COLUMNS} from "./index.constances.ts";
+import {useParams} from "react-router";
+import useFetchData from "../../../../../../../request/hooks/useFetchData.ts";
+import APIS from "../../../../../../../request/constances/apis.ts";
+import {useMemo} from "react";
+import {PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS_KEYS} from "../../../commitments/detail/index.constances.ts";
+import displayAvailableValues from "../../../../../../../utils/displayAvailableValues.ts";
+import type {IndividualBankingAccountDetailAccountSignatoriesResponseType} from "./index.types.ts";
+import RenderLogic from "../../../../../../../components/others/RenderLogic/RenderLogic.tsx";
 
 
 function IndividualBankingAccountDetailAccountSignatories() {
+
+
+  const {id: accountNumber} = useParams()
+
+  const {
+    data, isFetching, error
+  } = useFetchData<IndividualBankingAccountDetailAccountSignatoriesResponseType>({
+    axiosConfig: {
+      url: APIS.BANK_INFO_GET_ACCOUNT_SIGNATORS,
+      method: "POST",
+      params: {
+        accountNumber
+      }
+    }
+  })
+
+  const tableData = useMemo(function () {
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS_KEYS.CUSTOMER_NUMBER]: item.customerId,
+      [PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS_KEYS.CUSTOMER_TYPE]: item.legalTypeTitle,
+      [PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS_KEYS.NAME]: displayAvailableValues(item?.firstName, item?.lastName, item?.legalName || ''),
+      [PANEL_INDIVIDUAL_COMMITMENT_DETAIL_TABLE_COLUMNS_KEYS.NATIONAL_ID]: displayAvailableValues(item?.nationalID, item?.shenasnameId),
+    }))
+  }, [data])
+
+
   return (
-    <Table
-      columns={PANEL_INDIVIDUAL_ACCOUNT_DETAIL_SIGNATORIES_TABLE_COLUMNS}
-      data={[]}
-    />
+    <RenderLogic
+      isLoading={isFetching} error={error}
+      isEmpty={tableData?.length === 0}
+    >
+      <Table
+        columns={PANEL_INDIVIDUAL_ACCOUNT_DETAIL_SIGNATORIES_TABLE_COLUMNS}
+        data={tableData}
+      />
+    </RenderLogic>
   );
 }
 
---
+++ src/pages/panel/banking-info/individual/account/detail/accountSignatories/index.types.ts
@@ -0,0 +1,18 @@
+
+export type IndividualBankingAccountDetailAccountSignatoriesResponseType = {
+  customerId: number;
+  nationalID: number;
+  firstName: string;
+  lastName: string;
+  shenasnameId: number;
+  nationality: number;
+  nationalityTitle: string;
+  citizenshipTitle: string;
+  legalNationalID: number;
+  legalCustomerId: number;
+  legalName: string;
+  tradeName: string;
+  abbreviationName: string;
+  legalType: number;
+  legalTypeTitle: string;
+}[]
