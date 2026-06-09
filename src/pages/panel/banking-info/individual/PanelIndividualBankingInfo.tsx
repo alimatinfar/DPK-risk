d877ff23ce8cd2d90426a9f2a8b4dc5880d2dfe9
ali matinfar <a.matinfar@dpkharazmi.com>
2026-06-09 21:41:40 +0330

    [REFACTOR]: move renderObject of PanelIndividualBankingInfo.tsx to index.constances.tsx

--- src/pages/panel/banking-info/individual/PanelIndividualBankingInfo.tsx
+++ src/pages/panel/banking-info/individual/PanelIndividualBankingInfo.tsx
@@ -2,61 +2,19 @@ import PanelPageTitle from "../../layout/PanelPageTitle.tsx";
 import Tab from "../../../../components/others/Tab/Tab.tsx";
 import useActiveTab from "../../../../components/others/Tab/hooks/useActiveTab.ts";
 import {
+  PANEL_INDIVIDUAL_BANKING_INFO_PAGE_RENDER_OBJECT,
   PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS,
   PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS
 } from "./index.constances.tsx";
-import {lazy, useMemo} from "react";
 import TabContentRender from "../../../../components/others/Tab/TabContentRender.tsx";
 import Card from "../../../../components/others/Card/Card.tsx";
 import PanelIndividualBankingInfoTop from "./topInfo/PanelIndividualBankingInfoTop.tsx";
 
-const PanelIndividualBankingInfoTransactionHistory = lazy(() => import(
-  "./transactionHistory/PanelIndividualBankingInfoTransactionHistory.tsx"
-  ))
-const PanelIndividualBankingInfoFacilities = lazy(() => import(
-  "./facilities/PanelIndividualBankingInfoFacilities.tsx"
-  ))
-const PanelIndividualBankingInfoEServicePortal = lazy(() => import(
-  "./eServicePortal/PanelIndividualBankingInfoEServicePortal.tsx"
-  ))
-const PanelIndividualBankingInfoCommitments = lazy(() => import(
-  "./commitments/PanelIndividualBankingInfoCommitments.tsx"
-  ))
-const PanelIndividualBankingInfoCard = lazy(() => import(
-  "./card/PanelIndividualBankingInfoCard.tsx"
-  ))
-const PanelIndividualBankingInfoAccount = lazy(() => import(
-  "./account/PanelIndividualBankingInfoAccount.tsx"
-  ))
 
 
 function PanelIndividualBankingInfo() {
 
   const [activeTab, setActiveTab] = useActiveTab(PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.ACCOUNT)
-
-  const renderObject = useMemo(function () {
-      return {
-        [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.ACCOUNT]: (
-          <PanelIndividualBankingInfoAccount />
-        ),
-        [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.CARD]: (
-          <PanelIndividualBankingInfoCard />
-        ),
-        [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.FACILITIES]: (
-          <PanelIndividualBankingInfoFacilities />
-        ),
-        [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.COMMITMENTS]: (
-          <PanelIndividualBankingInfoCommitments />
-        ),
-        [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.E_SERVICE_PORTAL]: (
-          <PanelIndividualBankingInfoEServicePortal />
-        ),
-        [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.TRANSACTION_HISTORY]: (
-          <PanelIndividualBankingInfoTransactionHistory />
-        ),
-      }
-    }, [])
-
 
   return (
     <div className='flex flex-col gap-y-4'>
@@ -73,7 +31,8 @@ function PanelIndividualBankingInfo() {
         <PanelIndividualBankingInfoTop />
 
         <TabContentRender
-          renderObject={renderObject} activeTab={activeTab}
+          renderObject={PANEL_INDIVIDUAL_BANKING_INFO_PAGE_RENDER_OBJECT}
+          activeTab={activeTab}
         />
       </Card>
     </div>
--- src/pages/panel/banking-info/individual/index.constances.tsx
+++ src/pages/panel/banking-info/individual/index.constances.tsx
@@ -1,4 +1,25 @@
 import type {TabsProps} from "../../../../components/others/Tab/Tab.tsx";
+import {lazy} from "react";
+
+const PanelIndividualBankingInfoAccount = lazy(() => import(
+  "./account/PanelIndividualBankingInfoAccount.tsx"
+  ))
+const PanelIndividualBankingInfoCard = lazy(() => import(
+  "./card/PanelIndividualBankingInfoCard.tsx"
+  ))
+const PanelIndividualBankingInfoFacilities = lazy(() => import(
+  "./facilities/PanelIndividualBankingInfoFacilities.tsx"
+  ))
+const PanelIndividualBankingInfoCommitments = lazy(() => import(
+  "./commitments/PanelIndividualBankingInfoCommitments.tsx"
+  ))
+const PanelIndividualBankingInfoEServicePortal = lazy(() => import(
+  "./eServicePortal/PanelIndividualBankingInfoEServicePortal.tsx"
+  ))
+const PanelIndividualBankingInfoTransactionHistory = lazy(() => import(
+  "./transactionHistory/PanelIndividualBankingInfoTransactionHistory.tsx"
+  ))
+
 
 export const PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS = {
   ACCOUNT: 'account',
@@ -35,3 +56,24 @@ export const PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS: TabsProps['tabs'] = [
     title: 'سوابق تراکنش'
   },
 ]
+
+export const PANEL_INDIVIDUAL_BANKING_INFO_PAGE_RENDER_OBJECT = {
+  [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.ACCOUNT]: (
+    <PanelIndividualBankingInfoAccount />
+  ),
+  [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.CARD]: (
+    <PanelIndividualBankingInfoCard />
+  ),
+  [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.FACILITIES]: (
+    <PanelIndividualBankingInfoFacilities />
+  ),
+  [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.COMMITMENTS]: (
+    <PanelIndividualBankingInfoCommitments />
+  ),
+  [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.E_SERVICE_PORTAL]: (
+    <PanelIndividualBankingInfoEServicePortal />
+  ),
+  [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.TRANSACTION_HISTORY]: (
+    <PanelIndividualBankingInfoTransactionHistory />
+  ),
+}
