b9f751d25d4e65fb28b96b63e3b88b9a29ea251b
ali matinfar <a.matinfar@dpkharazmi.com>
2026-06-09 21:56:04 +0330

    [REFACTOR]: move renderObject of PanelEconomicInformationPage.tsx to index.constances.tsx

--- src/pages/panel/comprehensive-info/economic-information/PanelEconomicInformationPage.tsx
+++ src/pages/panel/comprehensive-info/economic-information/PanelEconomicInformationPage.tsx
@@ -2,37 +2,16 @@ import PanelPageTitle from "../../layout/PanelPageTitle.tsx";
 import Tab from "../../../../components/others/Tab/Tab.tsx";
 import useActiveTab from "../../../../components/others/Tab/hooks/useActiveTab.ts";
 import Card from "../../../../components/others/Card/Card.tsx";
-import {PANEL_ECONOMIC_INFORMATION_PAGE_TABS, PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS} from "./index.constances.ts";
+import {
+  PANEL_ECONOMIC_INFORMATION_PAGE_RENDER_OBJECT,
+  PANEL_ECONOMIC_INFORMATION_PAGE_TABS, PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS
+} from "./index.constances.tsx";
 import TabContentRender from "../../../../components/others/Tab/TabContentRender.tsx";
-import {lazy, useMemo} from "react";
 
-const PanelEconomicInformationPageEconomicActivity = lazy(() => import(
-  "./economic-activity/PanelEconomicInformationPageEconomicActivity.tsx"
-  ))
-const PanelEconomicInformationPagePrimarySourceOfIncome = lazy(() => import(
-  "./primary-source-of-income/PanelEconomicInformationPagePrimarySourceOfIncome.tsx"
-  ))
-const PanelEconomicInformationPageSecondarySourceOfIncome = lazy(() => import(
-  "./secondary-source-of-income/PanelEconomicInformationPageSecondarySourceOfIncome.tsx"
-  ))
 
 function PanelEconomicInformationPage() {
 
   const [activeTab, setActiveTab] = useActiveTab(PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.ECONOMIC_ACTIVITY)
-
-  const renderObject = useMemo(function () {
-    return {
-      [PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.ECONOMIC_ACTIVITY]: (
-        <PanelEconomicInformationPageEconomicActivity/>
-      ),
-      [PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.PRIMARY_SOURCE_OF_INCOME]: (
-        <PanelEconomicInformationPagePrimarySourceOfIncome/>
-      ),
-      [PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.SECONDARY_SOURCE_OF_INCOME]: (
-        <PanelEconomicInformationPageSecondarySourceOfIncome/>
-      ),
-    }
-  }, [])
 
   return (
     <div className='flex flex-col gap-y-4'>
@@ -47,7 +26,8 @@ function PanelEconomicInformationPage() {
 
       <Card>
         <TabContentRender
-          renderObject={renderObject} activeTab={activeTab}
+          renderObject={PANEL_ECONOMIC_INFORMATION_PAGE_RENDER_OBJECT}
+          activeTab={activeTab}
         />
       </Card>
     </div>
---
+++ src/pages/panel/comprehensive-info/economic-information/index.constances.tsx
@@ -0,0 +1,46 @@
+import type {TabsProps} from "../../../../components/others/Tab/Tab.tsx";
+import {lazy} from "react";
+
+const PanelEconomicInformationPageEconomicActivity = lazy(() => import(
+  "./economic-activity/PanelEconomicInformationPageEconomicActivity.tsx"
+  ))
+const PanelEconomicInformationPagePrimarySourceOfIncome = lazy(() => import(
+  "./primary-source-of-income/PanelEconomicInformationPagePrimarySourceOfIncome.tsx"
+  ))
+const PanelEconomicInformationPageSecondarySourceOfIncome = lazy(() => import(
+  "./secondary-source-of-income/PanelEconomicInformationPageSecondarySourceOfIncome.tsx"
+  ))
+
+
+export const PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS = {
+  ECONOMIC_ACTIVITY: 'economic-activity',
+  PRIMARY_SOURCE_OF_INCOME: 'primarySourceOfIncome',
+  SECONDARY_SOURCE_OF_INCOME: 'secondarySourceOfIncome',
+}
+
+export const PANEL_ECONOMIC_INFORMATION_PAGE_TABS: TabsProps['tabs'] = [
+  {
+    id: PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.ECONOMIC_ACTIVITY,
+    title: 'اطلاعات فعالیت اقتصادی'
+  },
+  {
+    id: PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.PRIMARY_SOURCE_OF_INCOME,
+    title: 'اطلاعات منبع درآمد اصلی'
+  },
+  {
+    id: PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.SECONDARY_SOURCE_OF_INCOME,
+    title: 'اطلاعات منابع درآمدی ثانویه'
+  },
+]
+
+export const PANEL_ECONOMIC_INFORMATION_PAGE_RENDER_OBJECT = {
+  [PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.ECONOMIC_ACTIVITY]: (
+    <PanelEconomicInformationPageEconomicActivity/>
+  ),
+  [PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.PRIMARY_SOURCE_OF_INCOME]: (
+    <PanelEconomicInformationPagePrimarySourceOfIncome/>
+  ),
+  [PANEL_ECONOMIC_INFORMATION_PAGE_TABS_KEYS.SECONDARY_SOURCE_OF_INCOME]: (
+    <PanelEconomicInformationPageSecondarySourceOfIncome/>
+  ),
+}
