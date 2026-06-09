0889e48a14166675713c91a710eca18e09a43743
ali matinfar <a.matinfar@dpkharazmi.com>
2026-06-09 21:53:06 +0330

    [REFACTOR]: move renderObject of PanelBankingInfoWithWithdrawalConditions.tsx to index.constance.tsx

--- src/pages/panel/banking-info/with-withdrawal-conditions/PanelBankingInfoWithWithdrawalConditions.tsx
+++ src/pages/panel/banking-info/with-withdrawal-conditions/PanelBankingInfoWithWithdrawalConditions.tsx
@@ -2,36 +2,19 @@ import useActiveTab from "../../../../components/others/Tab/hooks/useActiveTab.t
 import {
   PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS
 } from "../individual/index.constances.tsx";
-import {lazy, useMemo} from "react";
 import PanelPageTitle from "../../layout/PanelPageTitle.tsx";
 import Tab from "../../../../components/others/Tab/Tab.tsx";
 import Card from "../../../../components/others/Card/Card.tsx";
 import TabContentRender from "../../../../components/others/Tab/TabContentRender.tsx";
-import {PANEL_BANKING_INFO_WITH_WITHDRAWAL_CONDITIONS_PAGE_TABS} from "./index.constance.ts";
-
-const PanelBankingInfoWithWithdrawalConditionsAccount = lazy(() => import(
-  "./account/PanelBankingInfoWithWithdrawalConditionsAccount.tsx"
-  ));
-const PanelBankingInfoWithWithdrawalConditionsCard = lazy(() => import(
-  "./card/PanelBankingInfoWithWithdrawalConditionsCard.tsx"
-  ));
+import {
+  PANEL_BANKING_INFO_WITH_WITHDRAWAL_CONDITIONS_PAGE_RENDER_OBJECT,
+  PANEL_BANKING_INFO_WITH_WITHDRAWAL_CONDITIONS_PAGE_TABS
+} from "./index.constance.tsx";
 
 
 function PanelBankingInfoWithWithdrawalConditions() {
 
   const [activeTab, setActiveTab] = useActiveTab(PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.ACCOUNT)
-
-  const renderObject = useMemo(function () {
-    return {
-      [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.ACCOUNT]: (
-        <PanelBankingInfoWithWithdrawalConditionsAccount/>
-      ),
-      [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.CARD]: (
-        <PanelBankingInfoWithWithdrawalConditionsCard/>
-      ),
-    }
-  }, [])
-
 
   return (
     <div className='flex flex-col gap-y-4'>
@@ -46,7 +29,8 @@ function PanelBankingInfoWithWithdrawalConditions() {
 
       <Card className='flex flex-col gap-y-4'>
         <TabContentRender
-          renderObject={renderObject} activeTab={activeTab}
+          renderObject={PANEL_BANKING_INFO_WITH_WITHDRAWAL_CONDITIONS_PAGE_RENDER_OBJECT}
+          activeTab={activeTab}
         />
       </Card>
     </div>
---
+++ src/pages/panel/banking-info/with-withdrawal-conditions/index.constance.tsx
@@ -0,0 +1,29 @@
+import type {TabsProps} from "../../../../components/others/Tab/Tab.tsx";
+import {PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS} from "../individual/index.constances.tsx";
+import {lazy} from "react";
+const PanelBankingInfoWithWithdrawalConditionsAccount = lazy(() => import(
+  "./account/PanelBankingInfoWithWithdrawalConditionsAccount.tsx"
+  ));
+const PanelBankingInfoWithWithdrawalConditionsCard = lazy(() => import(
+  "./card/PanelBankingInfoWithWithdrawalConditionsCard.tsx"
+  ));
+
+export const PANEL_BANKING_INFO_WITH_WITHDRAWAL_CONDITIONS_PAGE_TABS: TabsProps['tabs'] = [
+  {
+    id: PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.ACCOUNT,
+    title: 'حساب'
+  },
+  {
+    id: PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.CARD,
+    title: 'کارت'
+  },
+]
+
+export const PANEL_BANKING_INFO_WITH_WITHDRAWAL_CONDITIONS_PAGE_RENDER_OBJECT = {
+  [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.ACCOUNT]: (
+    <PanelBankingInfoWithWithdrawalConditionsAccount/>
+  ),
+  [PANEL_INDIVIDUAL_BANKING_INFO_PAGE_TABS_KEYS.CARD]: (
+    <PanelBankingInfoWithWithdrawalConditionsCard/>
+  ),
+}
