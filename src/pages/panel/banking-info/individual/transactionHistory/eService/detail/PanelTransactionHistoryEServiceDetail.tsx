3d7a990a07d5a7331d0f93b3d0e018e66225ba0f
ali matinfar <a.matinfar@dpkharazmi.com>
2026-06-19 17:29:29 +0330

    [TASK]: added title of PanelTransactionHistoryEServicePeriodDetail.tsx

--- src/pages/panel/banking-info/individual/transactionHistory/eService/detail/PanelTransactionHistoryEServiceDetail.tsx
+++ src/pages/panel/banking-info/individual/transactionHistory/eService/detail/PanelTransactionHistoryEServiceDetail.tsx
@@ -3,11 +3,47 @@ import QUERY_PARAMS from "../../../../../../../constances/queryParams.ts";
 import PanelPageTitle from "../../../../../layout/PanelPageTitle.tsx";
 import Card from "../../../../../../../components/others/Card/Card.tsx";
 import Table from "../../../../../../../components/others/Table/Table.tsx";
+import {
+  PANEL_TRANSACTION_HISTORY_E_SERVICE_TABLE_COLUMNS, PANEL_TRANSACTION_HISTORY_E_SERVICE_TABLE_COLUMNS_KEYS,
+  PANEL_TRANSACTION_HISTORY_E_SERVICE_TABLE_FAKE_DATA
+} from "./index.constances.ts";
+import {useNavigate, useParams} from "react-router";
+import {useMemo} from "react";
+import TABLE_ACCESSORS from "../../../../../../../components/others/Table/constances/tableAccessors.ts";
+import {TableDetailAction} from "../../../../../../../components/others/Table/constances/actions/TableDetailAction.tsx";
+import ROUTER_LINKS from "../../../../../../../constances/routerLinks.ts";
+import type {PanelTransactionHistoryPeriodObjectType} from "../../onSiteMaxCount/index.types.ts";
+import getUrlWithParams from "../../../../../../../utils/getUrlWithParams.ts";
 
 
 function PanelTransactionHistoryEServiceDetail() {
 
   const eServiceLabel = useGetQueryParam(QUERY_PARAMS.NAME)
+
+  const navigate = useNavigate()
+
+  const {eServiceId} = useParams()
+
+  const tableData = useMemo(function () {
+
+    return PANEL_TRANSACTION_HISTORY_E_SERVICE_TABLE_FAKE_DATA?.map((item, index) => ({
+      ...item,
+      [TABLE_ACCESSORS.TD_ACTIONS_ACCESSOR]: [
+        TableDetailAction(() => {
+          const url = ROUTER_LINKS.PANEL_INDIVIDUAL_BANKING_INFORMATION_TRANSACTION_HISTORY_E_SERVICE_PERIOD_DETAIL(String(eServiceId), String(item.id))
+          const periodDateObject: Partial<PanelTransactionHistoryPeriodObjectType> = {
+            monthName: item[PANEL_TRANSACTION_HISTORY_E_SERVICE_TABLE_COLUMNS_KEYS.MONTH],
+            isCurrentMonth: index === 0
+          }
+          const params = {
+            [QUERY_PARAMS.PERIOD_DATE_OBJECT]: JSON.stringify(periodDateObject),
+            [QUERY_PARAMS.NAME]: eServiceLabel,
+          }
+          navigate(getUrlWithParams(url, params))
+        }, 'ریز تراکنش‌ها')
+      ],
+    }))
+  }, [])
 
   return (
     <div className='flex flex-col gap-y-4'>
@@ -16,11 +52,10 @@ function PanelTransactionHistoryEServiceDetail() {
       />
 
       <Card>
-        <></>
-        {/*<Table*/}
-        {/*  columns={}*/}
-        {/*  data={}*/}
-        {/*/>*/}
+        <Table
+          columns={PANEL_TRANSACTION_HISTORY_E_SERVICE_TABLE_COLUMNS}
+          data={tableData}
+        />
       </Card>
     </div>
   );
--- src/pages/panel/banking-info/individual/transactionHistory/eService/periodDetail/PanelTransactionHistoryEServicePeriodDetail.tsx
+++ src/pages/panel/banking-info/individual/transactionHistory/eService/periodDetail/PanelTransactionHistoryEServicePeriodDetail.tsx
@@ -1,7 +1,23 @@
+import PanelPageTitle from "../../../../../layout/PanelPageTitle.tsx";
+import useGetQueryParam from "../../../../../../../hooks/useGetQueryParam.ts";
+import QUERY_PARAMS from "../../../../../../../constances/queryParams.ts";
+import useGetTransactionHistoryPeriodDateObject
+  from "../../periodDetail/hooks/useGetTransactionHistoryPeriodDateObject.ts";
 
 function PanelTransactionHistoryEServicePeriodDetail() {
+
+  const eServiceLabel = useGetQueryParam(QUERY_PARAMS.NAME)
+
+  const {
+    isCurrentMonth, monthName
+  } = useGetTransactionHistoryPeriodDateObject()
+
   return (
-    <div></div>
+    <div className='flex flex-col gap-y-4'>
+      <PanelPageTitle
+        title={`تراکنش های ${eServiceLabel} ${`${monthName}${isCurrentMonth ? ' (ماه جاری)' : ''}`}`} hasBack
+      />
+    </div>
   );
 }
 
