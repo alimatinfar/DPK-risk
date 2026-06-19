576c34999ff8684b9f03753ff9f029bf7948bbab
ali matinfar <a.matinfar@dpkharazmi.com>
2026-06-19 17:11:26 +0330

    [TASK]: set title of PanelTransactionHistoryEServiceDetail.tsx

--- src/pages/panel/banking-info/individual/transactionHistory/eService/PanelTransactionHistoryEService.tsx
+++ src/pages/panel/banking-info/individual/transactionHistory/eService/PanelTransactionHistoryEService.tsx
@@ -1,6 +1,8 @@
 import {PANEL_TRANSACTION_HISTORY_E_SERVICE_PORTALS} from "./index.constances.tsx";
 import {Link} from "react-router";
 import ROUTER_LINKS from "../../../../../../constances/routerLinks.ts";
+import QUERY_PARAMS from "../../../../../../constances/queryParams.ts";
+import getUrlWithParams from "../../../../../../utils/getUrlWithParams.ts";
 
 function PanelTransactionHistoryEService() {
   return (
@@ -10,18 +12,26 @@ function PanelTransactionHistoryEService() {
       </p>
 
       <div className='flex flex-wrap justify-center gap-3'>
-        {PANEL_TRANSACTION_HISTORY_E_SERVICE_PORTALS.map(item => (
-          <Link
-            to={ROUTER_LINKS.PANEL_INDIVIDUAL_BANKING_INFORMATION_TRANSACTION_HISTORY_E_SERVICE_DETAIL(item.name)}
-            key={item.name}
-            className='flex flex-col items-center justify-center gap-y-2 bg-slate-50 hover:bg-slate-100 duration-200 cursor-pointer grow rounded-md border border-gray-300 p-4 min-w-40 max-w-50'
-          >
-            {item.icon}
-            <p>
-              {item.label}
-            </p>
-          </Link>
-        ))}
+        {PANEL_TRANSACTION_HISTORY_E_SERVICE_PORTALS.map(item => {
+
+          const linkUrl = ROUTER_LINKS.PANEL_INDIVIDUAL_BANKING_INFORMATION_TRANSACTION_HISTORY_E_SERVICE_DETAIL(item.name)
+          const params = {
+            [QUERY_PARAMS.NAME]: item.label
+          }
+
+          return (
+            <Link
+              to={getUrlWithParams(linkUrl, params)}
+              key={item.name}
+              className='flex flex-col items-center justify-center gap-y-2 bg-slate-50 hover:bg-slate-100 duration-200 cursor-pointer grow rounded-md border border-gray-300 p-4 min-w-40 max-w-50'
+            >
+              {item.icon}
+              <p>
+                {item.label}
+              </p>
+            </Link>
+          )
+        })}
       </div>
     </div>
   );
--- src/pages/panel/banking-info/individual/transactionHistory/eService/detail/PanelTransactionHistoryEServiceDetail.tsx
+++ src/pages/panel/banking-info/individual/transactionHistory/eService/detail/PanelTransactionHistoryEServiceDetail.tsx
@@ -1,8 +1,28 @@
+import useGetQueryParam from "../../../../../../../hooks/useGetQueryParam.ts";
+import QUERY_PARAMS from "../../../../../../../constances/queryParams.ts";
+import PanelPageTitle from "../../../../../layout/PanelPageTitle.tsx";
+import Card from "../../../../../../../components/others/Card/Card.tsx";
+import Table from "../../../../../../../components/others/Table/Table.tsx";
 
 
 function PanelTransactionHistoryEServiceDetail() {
+
+  const eServiceLabel = useGetQueryParam(QUERY_PARAMS.NAME)
+
   return (
-    <div></div>
+    <div className='flex flex-col gap-y-4'>
+      <PanelPageTitle
+        title={`تراکنش های ${eServiceLabel}`} hasBack
+      />
+
+      <Card>
+        <></>
+        {/*<Table*/}
+        {/*  columns={}*/}
+        {/*  data={}*/}
+        {/*/>*/}
+      </Card>
+    </div>
   );
 }
 
