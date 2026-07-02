5f054fd6070fdd648733902dcbe58121e705fdd2
ali matinfar <a.matinfar@dpkharazmi.com>
2026-07-02 09:54:37 +0330

    [REFACTOR]: move logic of PanelLegalRepresentativeInformationPageLegalRepresentative.tsx to custom hook

---
+++ src/pages/panel/comprehensive-info/legal-representative-information/legalRepresentative/hooks/usePanelLegalRepresentativeInformationPageLegalRepresentative.ts
@@ -0,0 +1,51 @@
+import getActivePersonData from "../../../../utils/getActivePersonData.ts";
+import useFetchData from "../../../../../../request/hooks/useFetchData.ts";
+import type {PanelLegalRepresentativeInformationResponseType} from "../index.types.ts";
+import APIS from "../../../../../../request/constances/apis.ts";
+import {PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS} from "../index.constances.ts";
+import displayDate from "../../../../../../utils/display/displayDate.ts";
+import {useMemo} from "react";
+
+
+function usePanelLegalRepresentativeInformationPageLegalRepresentative() {
+
+  const {activePersonData, isLegalBool, isNaturalBool} = getActivePersonData()
+
+  const {
+    data, isFetching, error
+  } = useFetchData<PanelLegalRepresentativeInformationResponseType>({
+    axiosConfig: {
+      url: APIS.GET_LEGAL_REPRESENTATIVE_INFO,
+      params: {
+        customerId: activePersonData?.customerId
+      }
+    }
+  })
+
+  const tableData = useMemo(function () {
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.CUSTOMER_NUMBER]: item?.customerIdStr,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.FIRST_NAME]: item?.firstName,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.LAST_NAME]: item?.lastName,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.NATIONAL_CODE_OR_FOREIGN_ID]: item?.nationalIDStr,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.RELATION]: item?.ratio,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.NATIONALITY]: item?.nationalityTitle,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.DOCUMENT_TYPE]: item?.certificateType,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.DOCUMENT_NUMBER_OR_CERTIFICATE_NUMBER]: item?.certificateNumber,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.AUTHENTICATION_CODE]: item?.authenticationCode,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.ISSUING_AUTHORITY]: item?.issuingAuthority,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.PLACE_OF_ISSUE]: item?.issuancePlace,
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.ISSUE_DATE]: displayDate(item?.dateOfIssuance),
+      [PANEL_LEGAL_REPRESENTATIVE_TABLE_COLUMNS_KEYS.EXPIRATION_DATE]: displayDate(item?.expirationDate),
+    }))
+  }, [data, isLegalBool, isNaturalBool])
+
+  return {
+    error, isFetching, tableData
+  }
+}
+
+export default usePanelLegalRepresentativeInformationPageLegalRepresentative;
