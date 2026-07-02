d08d5ff378a54747f84804b2f8403068f2458e1b
ali matinfar <a.matinfar@dpkharazmi.com>
2026-07-02 10:01:43 +0330

    [REFACTOR]: move logic of PanelIndividualBankingInfoCard.tsx  to custom hook

---
+++ src/pages/panel/banking-info/individual/card/hooks/usePanelIndividualBankingInfoCard.ts
@@ -0,0 +1,56 @@
+import {
+  PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS
+} from "../index.constances.tsx";
+import getActivePersonData from "../../../../utils/getActivePersonData.ts";
+import useFetchData from "../../../../../../request/hooks/useFetchData.ts";
+import type {PanelIndividualBankingInfoCardResponseType} from "../index.types.ts";
+import APIS from "../../../../../../request/constances/apis.ts";
+import {useMemo} from "react";
+import displayDate from "../../../../../../utils/display/displayDate.ts";
+
+function usePanelIndividualBankingInfoCard() {
+
+  const {activePersonData} = getActivePersonData();
+
+  const {
+    data, isFetching, error
+  } = useFetchData<PanelIndividualBankingInfoCardResponseType>({
+    axiosConfig: {
+      url: APIS.BANK_INFO_GET_CUSTOMER_CARDS,
+      method: "POST",
+      params: {
+        customerId: activePersonData?.customerId
+      }
+    }
+  })
+
+  const tableData = useMemo(function () {
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_NUMBER]: item?.cardNumber,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_ACCOUNT_NUMBER]: item?.cardAccount,
+      // TODO set tag for this field instead only value
+      // [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_STATUS]: (
+      //   <Tag text='فعال' color='green' variant='fade'/>
+      // ),
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_STATUS]: item?.cardStateTitle,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_TYPE]: item?.cardTypeTitle,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_DATE]: displayDate(item?.issueDate),
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.EXPIRY_DATE]: displayDate(item?.expirDate),
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_BRANCH_CODE]: item?.branchCode,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_BRANCH_NAME]: item?.branchName,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_REGION_CODE]: item?.areaCode,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_REGION_NAME]: item?.areaName,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUER_PERSONNEL_CODE]: item?.bankPersonnelCode,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUER_NAME]: item?.bankPersonnelName,
+    }))
+  }, [data])
+
+  return {
+    isFetching, error, tableData
+  }
+}
+
+export default usePanelIndividualBankingInfoCard;
