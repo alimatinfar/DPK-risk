b3bf91cb6a5f7a77527e7232c1cba0d1b7cbdc75
ali matinfar <a.matinfar@dpkharazmi.com>
2026-07-02 12:03:44 +0330

    [TASK]: added icons of PanelTransactionHistoryEServiceRender.tsx

--- src/pages/panel/banking-info/individual/transactionHistory/eService/PanelTransactionHistoryEServiceRender.tsx
+++ src/pages/panel/banking-info/individual/transactionHistory/eService/PanelTransactionHistoryEServiceRender.tsx
@@ -30,10 +30,11 @@ function PanelTransactionHistoryEServiceRender(
             <Link
               to={getUrlWithParams(linkUrl, params)}
               key={item.name}
-              className='flex flex-col items-center justify-center gap-y-2 bg-slate-50 hover:bg-slate-100 duration-200 cursor-pointer grow rounded-md border border-gray-300 p-4 min-w-40 max-w-50'
+              className='flex flex-col items-center justify-center gap-y-3 bg-gray-50 hover:bg-gray-100 duration-200 cursor-pointer grow rounded-lg border border-gray-200 p-4 min-w-50 max-w-60 min-h-24'
             >
-              {item.icon}
-              <p>
+              <item.icon />
+
+              <p className='font-medium'>
                 {item.label}
               </p>
             </Link>
--- src/pages/panel/banking-info/individual/transactionHistory/eService/index.constances.tsx
+++ src/pages/panel/banking-info/individual/transactionHistory/eService/index.constances.tsx
@@ -1,56 +1,68 @@
+import MobileBankIcon from "../../../../../../components/svg/eServices/MobileBankIcon.tsx";
+import InternetBankIcon from "../../../../../../components/svg/eServices/InternetBankIcon.tsx";
+import SepinoIcon from "../../../../../../components/svg/eServices/SepinoIcon.tsx";
+import USSDIcon from "../../../../../../components/svg/eServices/USSDIcon.tsx";
+import PoseIcon from "../../../../../../components/svg/eServices/PoseIcon.tsx";
+import IPGIcon from "../../../../../../components/svg/eServices/IPGIcon.tsx";
+import BulkDepositIcon from "../../../../../../components/svg/eServices/BulkDepositIcon.tsx";
+import MojIcon from "../../../../../../components/svg/eServices/MojIcon.tsx";
+import OpenBankingIcon from "../../../../../../components/svg/eServices/OpenBankingIcon.tsx";
+import GoldBibIcon from "../../../../../../components/svg/eServices/GoldBibIcon.tsx";
+import SilverBibIcon from "../../../../../../components/svg/eServices/SilverBibIcon.tsx";
+
 export const PANEL_TRANSACTION_HISTORY_E_SERVICE_PORTALS = [
   {
-    icon: <></>,
+    icon: MobileBankIcon,
     name: '1',
     label: 'همراه بانک',
   },
   {
-    icon: <></>,
+    icon: InternetBankIcon,
     name: '2',
     label: 'اینترنت بانک',
   },
   {
-    icon: <></>,
+    icon: SepinoIcon,
     name: '3',
     label: 'سپینو',
   },
   {
-    icon: <></>,
+    icon: USSDIcon,
     name: '4',
     label: 'ussd',
   },
   {
-    icon: <></>,
+    icon: PoseIcon,
     name: '5',
     label: 'pose',
   },
   {
-    icon: <></>,
+    icon: IPGIcon,
     name: '6',
     label: 'ipg',
   },
   {
-    icon: <></>,
+    icon: BulkDepositIcon,
     name: '7',
     label: 'واریز جمعی',
   },
   {
-    icon: <></>,
+    icon: MojIcon,
     name: '8',
     label: 'موج',
   },
   {
-    icon: <></>,
+    icon: OpenBankingIcon,
     name: '9',
     label: 'بانکداری باز',
   },
   {
-    icon: <></>,
+    icon: GoldBibIcon,
     name: '10',
     label: 'gold-bib',
   },
   {
-    icon: <></>,
+    icon: SilverBibIcon,
     name: '11',
     label: 'silver-bib',
   },
