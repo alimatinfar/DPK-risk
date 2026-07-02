57cfa4f1f28d3ee5bc6275f40ded04f4e700a230
ali matinfar <a.matinfar@dpkharazmi.com>
2026-07-02 13:56:32 +0330

    [TASK]: move logic of PanelBankingInfoWithWithdrawalConditionsCard.tsx to custom hook

---
+++ src/pages/panel/banking-info/with-withdrawal-conditions/card/hooks/usePanelBankingInfoWithWithdrawalConditionsCard.ts
@@ -0,0 +1,56 @@
+import getActivePersonData from "../../../../utils/getActivePersonData.ts";
+import type {PanelBankingInfoWithWithdrawalConditionsCardResponseType} from "../index.types.ts";
+import APIS from "../../../../../../request/constances/apis.ts";
+import {useMemo} from "react";
+import {PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS} from "../../../individual/card/index.constances.tsx";
+import displayDate from "../../../../../../utils/display/displayDate.ts";
+import useFetchData from "../../../../../../request/hooks/useFetchData.ts";
+
+
+function usePanelBankingInfoWithWithdrawalConditionsCard() {
+
+  const {activePersonData} = getActivePersonData();
+
+  const {
+    data, isFetching, error
+  } = useFetchData<PanelBankingInfoWithWithdrawalConditionsCardResponseType>({
+    axiosConfig: {
+      url: APIS.BANK_INFO_GET_SPECIAL_CONDITIONS_CARDS,
+      method: "POST",
+      params: {
+        customerId: activePersonData?.customerId
+      }
+    }
+  })
+
+  const tableData = useMemo(function () {
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_NUMBER]: item?.cardNumber,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_ACCOUNT_NUMBER]: item?.cardAccount,
+      // TODO set tag for this field instead only value
+      // [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_STATUS]: (
+      //   <Tag text='فعال' color='green' variant='fade'/>
+      // ),
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_STATUS]: item?.cardStateTitle,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.CARD_TYPE]: item?.cardTypeTitle,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_DATE]: displayDate(item?.issueDate),
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.EXPIRY_DATE]: displayDate(item?.expirDate),
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_BRANCH_CODE]: item?.branchCode,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_BRANCH_NAME]: item?.branchName,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_REGION_CODE]: item?.areaCode,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUE_REGION_NAME]: item?.areaName,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUER_PERSONNEL_CODE]: item?.bankPersonnelCode,
+      [PANEL_INDIVIDUAL_CARD_INFO_TABLE_COLUMNS_KEYS.ISSUER_NAME]: item?.bankPersonnelName,
+    }))
+  }, [data])
+
+
+  return {
+    isFetching, error, tableData
+  }
+}
+
+export default usePanelBankingInfoWithWithdrawalConditionsCard;
