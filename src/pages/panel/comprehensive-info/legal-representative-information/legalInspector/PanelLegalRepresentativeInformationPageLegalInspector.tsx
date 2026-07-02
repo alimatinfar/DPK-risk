ea959078257c4673079965112f045eef8382dcb5
ali matinfar <a.matinfar@dpkharazmi.com>
2026-07-02 09:50:52 +0330

    [REFACTOR]: move logic of PanelLegalRepresentativeInformationPageLegalInspector.tsx to custom hook

--- src/pages/panel/comprehensive-info/legal-representative-information/legalInspector/PanelLegalRepresentativeInformationPageLegalInspector.tsx
+++ src/pages/panel/comprehensive-info/legal-representative-information/legalInspector/PanelLegalRepresentativeInformationPageLegalInspector.tsx
@@ -1,45 +1,17 @@
 import {
   PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS,
-  PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS
 } from "../statutoryAuditor/index.constances.ts";
 import Table from "../../../../../components/others/Table/Table.tsx";
-import getActivePersonData from "../../../utils/getActivePersonData.ts";
-import useFetchData from "../../../../../request/hooks/useFetchData.ts";
-import APIS from "../../../../../request/constances/apis.ts";
-import {useMemo} from "react";
-import type {PanelLegalRepresentativeLegalInspectorResponseType} from "./index.types.ts";
 import RenderLogic from "../../../../../components/others/RenderLogic/RenderLogic.tsx";
-import displayDate from "../../../../../utils/display/displayDate.ts";
+import usePanelLegalRepresentativeInformationPageLegalInspector
+  from "./hooks/usePanelLegalRepresentativeInformationPageLegalInspector.ts";
 
 
 function PanelLegalRepresentativeInformationPageLegalInspector() {
 
-  const {activePersonData} = getActivePersonData()
-
   const {
-    data, isFetching, error
-  } = useFetchData<PanelLegalRepresentativeLegalInspectorResponseType>({
-    axiosConfig: {
-      url: APIS.GET_LEGAL_INSPECTOR_INFO,
-      params: {
-        customerId: activePersonData?.customerId
-      }
-    }
-  })
-
-  const tableData = useMemo(function () {
-    if (!data?.data) return []
-
-    return data?.data?.map((item, index) => ({
-      id: index,
-      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.CUSTOMER_NUMBER]: item?.customerIdStr,
-      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.FIRST_NAME]: item?.firstName,
-      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.LAST_NAME]: item?.lastName,
-      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.NATIONAL_CODE]: item?.nationalIDStr,
-      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.FROM_DATE]: displayDate(item?.fromDate),
-      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.TO_DATE]: displayDate(item?.toDate),
-    }))
-  }, [data])
+    error, isFetching, tableData
+  } = usePanelLegalRepresentativeInformationPageLegalInspector()
 
   return (
     <RenderLogic
---
+++ src/pages/panel/comprehensive-info/legal-representative-information/legalInspector/hooks/usePanelLegalRepresentativeInformationPageLegalInspector.ts
@@ -0,0 +1,46 @@
+import getActivePersonData from "../../../../utils/getActivePersonData.ts";
+import useFetchData from "../../../../../../request/hooks/useFetchData.ts";
+import APIS from "../../../../../../request/constances/apis.ts";
+import {useMemo} from "react";
+import type {PanelLegalRepresentativeLegalInspectorResponseType} from "../index.types.ts";
+import {
+  PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS
+} from "../../statutoryAuditor/index.constances.ts";
+import displayDate from "../../../../../../utils/display/displayDate.ts";
+
+
+function usePanelLegalRepresentativeInformationPageLegalInspector() {
+
+  const {activePersonData} = getActivePersonData()
+
+  const {
+    data, isFetching, error
+  } = useFetchData<PanelLegalRepresentativeLegalInspectorResponseType>({
+    axiosConfig: {
+      url: APIS.GET_LEGAL_INSPECTOR_INFO,
+      params: {
+        customerId: activePersonData?.customerId
+      }
+    }
+  })
+
+  const tableData = useMemo(function () {
+    if (!data?.data) return []
+
+    return data?.data?.map((item, index) => ({
+      id: index,
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.CUSTOMER_NUMBER]: item?.customerIdStr,
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.FIRST_NAME]: item?.firstName,
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.LAST_NAME]: item?.lastName,
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.NATIONAL_CODE]: item?.nationalIDStr,
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.FROM_DATE]: displayDate(item?.fromDate),
+      [PANEL_LEGAL_REPRESENTATIVE_STATUTORY_AUDITOR_TABLE_COLUMNS_KEYS.TO_DATE]: displayDate(item?.toDate),
+    }))
+  }, [data])
+
+  return {
+    error, isFetching, tableData
+  }
+}
+
+export default usePanelLegalRepresentativeInformationPageLegalInspector;
