8f9499a78ac0a3e54e6a10b68fa6b0a8c0a90b15
ali matinfar <a.matinfar@dpkharazmi.com>
2026-08-14 12:22:54 +0330

    [REFACTOR]: created AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalContent.tsx and refactored AdminHighRiskIndividualsLettersDetailDocumentsDeleteModal.tsx

--- src/pages/admin/high-risk-individuals/letters/detail/documents/deleteModal/AdminHighRiskIndividualsLettersDetailDocumentsDeleteModal.tsx
+++ src/pages/admin/high-risk-individuals/letters/detail/documents/deleteModal/AdminHighRiskIndividualsLettersDetailDocumentsDeleteModal.tsx
@@ -1,11 +1,7 @@
 import Modal, {type ModalProps} from "../../../../../../../components/others/Modal/Modal.tsx";
-import DescriptionField from "../../../../FormFields/DescriptionField/DescriptionField.tsx";
-import ReactHookFormWrapper
-  from "../../../../../../../components/Form/FormLayout/ReactHookFormWrapper/ReactHookFormWrapper.tsx";
-import Button from "../../../../../../../components/Form/Button/Button.tsx";
 import type {AdminHighRiskIndividualsLettersDetailDocumentType} from "../index.types.ts";
-import useAdminHighRiskIndividualsLettersDetailDocumentsDeleteModal
-  from "./hooks/useAdminHighRiskIndividualsLettersDetailDocumentsDeleteModal.ts";
+import AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalContent
+  from "./AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalContent.tsx";
 
 
 export type AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalProps = {
@@ -16,36 +12,14 @@ function AdminHighRiskIndividualsLettersDetailDocumentsDeleteModal(
   {open, onClose, modalState}: AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalProps
 ) {
 
-  const {
-    formMethods, onSubmit
-  } = useAdminHighRiskIndividualsLettersDetailDocumentsDeleteModal({
-    modalState
-  })
-
   return (
     <Modal
       open={open} onClose={onClose} title='حذف مستند'
       description='ايا از انجام عمليات حذف اطمينان داريد؟'
     >
-      <ReactHookFormWrapper
-        formMethods={formMethods} onSubmit={onSubmit}
-      >
-        <DescriptionField/>
-
-        <div className='grid grid-cols-2 gap-x-4 mt-2'>
-          <Button
-            variant='default' color='white'
-          >
-            انصراف
-          </Button>
-
-          <Button
-            color='red' type='submit'
-          >
-            حذف
-          </Button>
-        </div>
-      </ReactHookFormWrapper>
+      <AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalContent
+        cancelAction={onClose} modalState={modalState}
+      />
     </Modal>
   );
 }
---
+++ src/pages/admin/high-risk-individuals/letters/detail/documents/deleteModal/AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalContent.tsx
@@ -0,0 +1,50 @@
+import ReactHookFormWrapper
+  from "../../../../../../../components/Form/FormLayout/ReactHookFormWrapper/ReactHookFormWrapper.tsx";
+import DescriptionField from "../../../../FormFields/DescriptionField/DescriptionField.tsx";
+import Button from "../../../../../../../components/Form/Button/Button.tsx";
+import useAdminHighRiskIndividualsLettersDetailDocumentsDeleteModal
+  from "./hooks/useAdminHighRiskIndividualsLettersDetailDocumentsDeleteModal.ts";
+import type {
+  AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalProps
+} from "./AdminHighRiskIndividualsLettersDetailDocumentsDeleteModal.tsx";
+
+
+type Props = {
+  cancelAction: () => void;
+} & Pick<AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalProps, 'modalState'>;
+
+function AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalContent(
+  {cancelAction, modalState}: Props
+) {
+
+  const {
+    formMethods, onSubmit
+  } = useAdminHighRiskIndividualsLettersDetailDocumentsDeleteModal({
+    modalState
+  })
+
+  return (
+    <ReactHookFormWrapper
+      formMethods={formMethods} onSubmit={onSubmit}
+    >
+      <DescriptionField/>
+
+      <div className='grid grid-cols-2 gap-x-4 mt-2'>
+        <Button
+          variant='default' color='white'
+          onClick={cancelAction}
+        >
+          انصراف
+        </Button>
+
+        <Button
+          color='red' type='submit'
+        >
+          حذف
+        </Button>
+      </div>
+    </ReactHookFormWrapper>
+  );
+}
+
+export default AdminHighRiskIndividualsLettersDetailDocumentsDeleteModalContent;
