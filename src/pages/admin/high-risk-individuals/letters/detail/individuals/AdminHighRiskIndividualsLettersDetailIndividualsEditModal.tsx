dd259c16a8e5a41a8649a28866ac90e188ed63cb
ali matinfar <a.matinfar@dpkharazmi.com>
2026-08-14 09:29:29 +0330

    [REFACTOR]: refactored AdminHighRiskIndividualsLettersDetailIndividualsEditModal.tsx and moved the logics of this component to custom hook

--- src/pages/admin/high-risk-individuals/letters/detail/individuals/AdminHighRiskIndividualsLettersDetailIndividualsEditModal.tsx
+++ src/pages/admin/high-risk-individuals/letters/detail/individuals/AdminHighRiskIndividualsLettersDetailIndividualsEditModal.tsx
@@ -1,42 +1,26 @@
 import Modal, {type ModalProps} from "../../../../../../components/others/Modal/Modal.tsx";
 import type {AdminHighRiskIndividualsLettersDetailIndividualsDataItemType} from "./index.types.ts";
-import {useMemo} from "react";
-import {SEARCH_PAGE_FORM_PERSON_TYPE_KEYS} from "../../../../../search/form/SearchPageForm.constances.ts";
 import ReactHookFormWrapper
   from "../../../../../../components/Form/FormLayout/ReactHookFormWrapper/ReactHookFormWrapper.tsx";
 import ExpireDateField from "../../../FormFields/ExpireDateField/ExpireDateField.tsx";
-import useReactHookFormWrapper
-  from "../../../../../../components/Form/FormLayout/ReactHookFormWrapper/hooks/useReactHookFormWrapper.ts";
-import {expireDateFieldName, type ExpireDateFieldType} from "../../../FormFields/ExpireDateField/index.constances.ts";
 import Button from "../../../../../../components/Form/Button/Button.tsx";
+import useAdminHighRiskIndividualsLettersDetailIndividualsEditModal
+  from "./hooks/useAdminHighRiskIndividualsLettersDetailIndividualsEditModal.ts";
 
-type FormDataType = {
-  [expireDateFieldName]: ExpireDateFieldType;
-}
 
-type Props = {
+export type AdminHighRiskIndividualsLettersDetailIndividualsEditModalProps = {
   modalState: AdminHighRiskIndividualsLettersDetailIndividualsDataItemType | boolean
 } & Pick<ModalProps, 'open' | 'onClose'>
 
 function AdminHighRiskIndividualsLettersDetailIndividualsEditModal(
-  {open, onClose, modalState}: Props
+  {open, onClose, modalState}: AdminHighRiskIndividualsLettersDetailIndividualsEditModalProps
 ) {
 
-  function onSubmitHandler(formData: FormDataType) {
-    console.log({formData})
-  }
-
   const {
-    formMethods, onSubmit
-  } = useReactHookFormWrapper<FormDataType>({
-    onSubmitHandler
+    personTitle, formMethods, onSubmit
+  } = useAdminHighRiskIndividualsLettersDetailIndividualsEditModal({
+    modalState
   })
-
-  const personTitle = useMemo(function () {
-    if (typeof modalState === 'boolean') return ''
-
-    return `${modalState?.name} ${modalState?.type === SEARCH_PAGE_FORM_PERSON_TYPE_KEYS.LEGAL.name ? '' : modalState?.lastName}`
-  }, [modalState])
 
   return (
     <Modal
---
+++ src/pages/admin/high-risk-individuals/letters/detail/individuals/hooks/useAdminHighRiskIndividualsLettersDetailIndividualsEditModal.ts
@@ -0,0 +1,44 @@
+import useReactHookFormWrapper
+  from "../../../../../../../components/Form/FormLayout/ReactHookFormWrapper/hooks/useReactHookFormWrapper.ts";
+import {useMemo} from "react";
+import {SEARCH_PAGE_FORM_PERSON_TYPE_KEYS} from "../../../../../../search/form/SearchPageForm.constances.ts";
+import type {
+  AdminHighRiskIndividualsLettersDetailIndividualsEditModalProps
+} from "../AdminHighRiskIndividualsLettersDetailIndividualsEditModal.tsx";
+import {
+  expireDateFieldName,
+  type ExpireDateFieldType
+} from "../../../../FormFields/ExpireDateField/index.constances.ts";
+
+type FormDataType = {
+  [expireDateFieldName]: ExpireDateFieldType;
+}
+
+function useAdminHighRiskIndividualsLettersDetailIndividualsEditModal(
+  {
+    modalState
+  }: Pick<AdminHighRiskIndividualsLettersDetailIndividualsEditModalProps, 'modalState'>
+) {
+
+  function onSubmitHandler(formData: FormDataType) {
+    console.log({formData})
+  }
+
+  const {
+    formMethods, onSubmit
+  } = useReactHookFormWrapper<FormDataType>({
+    onSubmitHandler
+  })
+
+  const personTitle = useMemo(function () {
+    if (typeof modalState === 'boolean') return ''
+
+    return `${modalState?.name} ${modalState?.type === SEARCH_PAGE_FORM_PERSON_TYPE_KEYS.LEGAL.name ? '' : modalState?.lastName}`
+  }, [modalState])
+
+  return {
+    personTitle, formMethods, onSubmit
+  }
+}
+
+export default useAdminHighRiskIndividualsLettersDetailIndividualsEditModal;
