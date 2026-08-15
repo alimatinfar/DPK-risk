a494b4bc99940d289f1b745294da30e196a17a8d
ali matinfar <a.matinfar@dpkharazmi.com>
2026-08-15 20:13:02 +0330

    [REFACTOR]: move logics of AdminHighRiskIndividualsDetailPage.tsx to custom hook

--- src/pages/admin/high-risk-individuals/individuals/detail/AdminHighRiskIndividualsDetailPage.tsx
+++ src/pages/admin/high-risk-individuals/individuals/detail/AdminHighRiskIndividualsDetailPage.tsx
@@ -1,77 +1,21 @@
-import {useParams} from "react-router";
-import useGetQueryParam from "../../../../../hooks/useGetQueryParam.ts";
-import QUERY_PARAMS from "../../../../../constances/queryParams.ts";
 import AdminPageTitle from "../../../layout/AdminPageTitle.tsx";
 import CardWithHeader from "../../../../../components/others/Card/CardWithHeader/CardWithHeader.tsx";
 import {ADMIN_HIGH_RISK_INDIVIDUAL_RELATED_LETTERS_FAKE_DATE} from "./index.constances.ts";
 import AdminHighRiskIndividualsExitLetterContainer
   from "../../letters/detail/individuals/AdminHighRiskIndividualsExitLetterContainer.tsx";
-import type {
-  AdminHighRiskIndividualRelatedLetterType,
-  AdminHighRiskIndividualRelatedShareLetterType
-} from "./index.types.ts";
-import {letterNumberFieldName} from "../../FormFields/LetterNumberField/index.constances.ts";
-import {announceReferenceFieldName} from "../../FormFields/AnnouncingReferenceField/index.constances.ts";
-import {letterDateFieldName} from "../../FormFields/LetterDateField/index.constances.ts";
-import {descriptionFieldName} from "../../FormFields/DescriptionField/index.constances.ts";
-import {expireDateFieldName} from "../../FormFields/ExpireDateField/index.constances.ts";
-import AdminHighRiskIndividualsEntryReasonsTags
-  from "../../letters/detail/individuals/AdminHighRiskIndividualsEntryReasonsTags.tsx";
 import AdminHighRiskIndividualsFieldsWithActions
   from "../../letters/detail/individuals/AdminHighRiskIndividualsFieldsWithActions.tsx";
 import Button from "../../../../../components/Form/Button/Button.tsx";
 import LogoutIcon from "../../../../../components/svg/LogoutIcon.tsx";
 import CardRightRed from "../../../../../components/others/Card/CardRightRed.tsx";
+import useAdminHighRiskIndividualsDetailPage from "./hooks/useAdminHighRiskIndividualsDetailPage.tsx";
 
-
-type GetFieldsIsExitLetter = {
-  isExit: true;
-  item: AdminHighRiskIndividualRelatedShareLetterType;
-}
-
-type GetFieldsIsMainLetter = {
-  isExit: false;
-  item: AdminHighRiskIndividualRelatedLetterType;
-}
-
-type GetFieldsType = GetFieldsIsExitLetter | GetFieldsIsMainLetter
 
 function AdminHighRiskIndividualsDetailPage() {
 
-  const {id: customNumber} = useParams()
-  const name = useGetQueryParam(QUERY_PARAMS.NAME)
-
-  function getTopFields({item, isExit}: GetFieldsType) {
-    return [
-      {
-        label: 'شماره نامه', value: item?.[letterNumberFieldName]
-      },
-      {
-        label: 'مرجع', value: item?.[announceReferenceFieldName]
-      },
-      {
-        label: 'تاریخ نامه', value: item?.[letterDateFieldName]
-      },
-      ...isExit ? [] : [{
-        label: 'تاریخ اعتبار', value: item?.[expireDateFieldName]
-      }],
-    ]
-  }
-
-  function getBottomFields({item, isExit}: GetFieldsType) {
-    return [
-      {
-        label: 'توضیحات نامه', value: item?.[descriptionFieldName]
-      },
-      ...isExit ? [] : [{
-        label: 'دلیل ورود', value: (
-          <AdminHighRiskIndividualsEntryReasonsTags
-            entryReasons={item.entryReasons}
-          />
-        )
-      }],
-    ]
-  }
+  const {
+    name, getTopFields, getBottomFields
+  } = useAdminHighRiskIndividualsDetailPage()
 
   return (
     <div className='flex flex-col gap-y-4'>
---
+++ src/pages/admin/high-risk-individuals/individuals/detail/hooks/useAdminHighRiskIndividualsDetailPage.tsx
@@ -0,0 +1,71 @@
+import {useParams} from "react-router";
+import useGetQueryParam from "../../../../../../hooks/useGetQueryParam.ts";
+import QUERY_PARAMS from "../../../../../../constances/queryParams.ts";
+import {letterNumberFieldName} from "../../../FormFields/LetterNumberField/index.constances.ts";
+import {announceReferenceFieldName} from "../../../FormFields/AnnouncingReferenceField/index.constances.ts";
+import {letterDateFieldName} from "../../../FormFields/LetterDateField/index.constances.ts";
+import {expireDateFieldName} from "../../../FormFields/ExpireDateField/index.constances.ts";
+import {descriptionFieldName} from "../../../FormFields/DescriptionField/index.constances.ts";
+import AdminHighRiskIndividualsEntryReasonsTags
+  from "../../../letters/detail/individuals/AdminHighRiskIndividualsEntryReasonsTags.tsx";
+import type {
+  AdminHighRiskIndividualRelatedLetterType,
+  AdminHighRiskIndividualRelatedShareLetterType
+} from "../index.types.ts";
+
+
+type GetFieldsIsExitLetter = {
+  isExit: true;
+  item: AdminHighRiskIndividualRelatedShareLetterType;
+}
+
+type GetFieldsIsMainLetter = {
+  isExit: false;
+  item: AdminHighRiskIndividualRelatedLetterType;
+}
+
+type GetFieldsType = GetFieldsIsExitLetter | GetFieldsIsMainLetter
+
+function useAdminHighRiskIndividualsDetailPage() {
+
+  const {id: customNumber} = useParams()
+  const name = useGetQueryParam(QUERY_PARAMS.NAME)
+
+  function getTopFields({item, isExit}: GetFieldsType) {
+    return [
+      {
+        label: 'شماره نامه', value: item?.[letterNumberFieldName]
+      },
+      {
+        label: 'مرجع', value: item?.[announceReferenceFieldName]
+      },
+      {
+        label: 'تاریخ نامه', value: item?.[letterDateFieldName]
+      },
+      ...isExit ? [] : [{
+        label: 'تاریخ اعتبار', value: item?.[expireDateFieldName]
+      }],
+    ]
+  }
+
+  function getBottomFields({item, isExit}: GetFieldsType) {
+    return [
+      {
+        label: 'توضیحات نامه', value: item?.[descriptionFieldName]
+      },
+      ...isExit ? [] : [{
+        label: 'دلیل ورود', value: (
+          <AdminHighRiskIndividualsEntryReasonsTags
+            entryReasons={item.entryReasons}
+          />
+        )
+      }],
+    ]
+  }
+
+  return {
+    name, getTopFields, getBottomFields
+  }
+}
+
+export default useAdminHighRiskIndividualsDetailPage;
