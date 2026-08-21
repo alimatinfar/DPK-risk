7662d3060356030c2a0fa742cf4ff2cf8e379719
a.matinfar <a.matinfar@dpkharazmi.com>
2026-08-21 14:14:22 +0330

    [TASK]: added top section of AdminHighRiskFormStep4Form.tsx

--- src/pages/admin/high-risk-individuals/form/steps/step4/AdminHighRiskFormStep4.tsx
+++ src/pages/admin/high-risk-individuals/form/steps/step4/AdminHighRiskFormStep4.tsx
@@ -2,8 +2,20 @@ import AdminHighRiskIndividualsFormButtons from "../../AdminHighRiskIndividualsF
 import Card from "../../../../../../components/others/Card/Card";
 import AdminHighRiskFormStep4Individuals from "./AdminHighRiskFormStep4Individuals";
 import AdminHighRiskFormStep4Form from "./AdminHighRiskFormStep4Form";
+import useReactHookFormWrapper from "../../../../../../components/Form/FormLayout/ReactHookFormWrapper/hooks/useReactHookFormWrapper.ts";
 
 function AdminHighRiskFormStep4() {
+
+  const {
+    formMethods, onSubmit
+  } = useReactHookFormWrapper({
+    onSubmitHandler
+  })
+
+  function onSubmitHandler() {
+
+  }
+
   return (
     <div className='flex flex-col gap-y-4'>
       <Card
@@ -12,7 +24,9 @@ function AdminHighRiskFormStep4() {
       >
         <AdminHighRiskFormStep4Individuals />
 
-        <AdminHighRiskFormStep4Form />
+        <AdminHighRiskFormStep4Form
+          formMethods={formMethods} onSubmit={onSubmit}
+        />
       </Card>
 
       <AdminHighRiskIndividualsFormButtons/>
--- src/pages/admin/high-risk-individuals/form/steps/step4/AdminHighRiskFormStep4Form.tsx
+++ src/pages/admin/high-risk-individuals/form/steps/step4/AdminHighRiskFormStep4Form.tsx
@@ -1,6 +1,26 @@
-function AdminHighRiskFormStep4Form() {
+import ReactHookFormWrapper, {
+  type ReactHookFormWrapperProps
+} from "../../../../../../components/Form/FormLayout/ReactHookFormWrapper/ReactHookFormWrapper";
+import ExpireDateField from "../../../FormFields/ExpireDateField/ExpireDateField";
+import EntryReasonsField from "../../../FormFields/EntryReasonsField/EntryReasonsField";
+
+
+type Props = Pick<ReactHookFormWrapperProps, 'formMethods' | 'onSubmit'>
+
+function AdminHighRiskFormStep4Form(
+  {formMethods, onSubmit}: Props
+) {
   return (
-    <div></div>
+    <div className='flex-1 p-4'>
+      <ReactHookFormWrapper
+        formMethods={formMethods} onSubmit={onSubmit}
+        className='border-b border-gray-200 grid grid-cols-2 gap-4'
+      >
+        <ExpireDateField/>
+
+        <EntryReasonsField/>
+      </ReactHookFormWrapper>
+    </div>
   );
 }
 
