cfa66e01dd56052f7770005afef1f678d7c15835
ali matinfar <a.matinfar@dpkharazmi.com>
2026-09-07 18:04:23 +0330

    [TASK]: refactored AdminHighRiskFormStep3SearchUserForm.tsx

--- src/pages/admin/high-risk-individuals/form/steps/step3/AdminHighRiskFormStep3SearchUserForm.tsx
+++ src/pages/admin/high-risk-individuals/form/steps/step3/AdminHighRiskFormStep3SearchUserForm.tsx
@@ -1,14 +1,12 @@
 import TabContentRender from "../../../../../../components/others/Tab/TabContentRender.tsx";
-import ReactHookFormWrapper
-  , {
+import {
   type ReactHookFormWrapperProps
 } from "../../../../../../components/Form/FormLayout/ReactHookFormWrapper/ReactHookFormWrapper.tsx";
-import Button from "../../../../../../components/Form/Button/Button.tsx";
-import SearchIcon from "../../../../../../components/svg/SearchIcon.tsx";
 import {
   ADMIN_HIGH_RISK_INDIVIDUALS_FORM_STEP3_TABS_RENDER_OBJECT
 } from "./index.constances.tsx";
 import type {TabType} from "../../../../../../components/others/Tab/Tab";
+import SearchFormWrapper from "../../../../../../components/Form/FormLayout/SearchFormWrapper.tsx";
 
 
 type Props = {
@@ -20,23 +18,14 @@ function AdminHighRiskFormStep3SearchUserForm(
   {activeTab, formMethods, onSubmit, loading}: Props
 ) {
   return (
-    <ReactHookFormWrapper
-      formMethods={formMethods} onSubmit={onSubmit} className='grid grid-cols-3 gap-4'
+    <SearchFormWrapper
+      formMethods={formMethods} onSubmit={onSubmit} loading={loading}
     >
       <TabContentRender
         renderObject={ADMIN_HIGH_RISK_INDIVIDUALS_FORM_STEP3_TABS_RENDER_OBJECT}
         activeTab={activeTab}
       />
-
-      <div className='flex justify-end items-center col-start-3 pt-2.5 min-h-24'>
-        <Button
-          variant='outlined' rightIcon={<SearchIcon textColor='text-primary' />} type='submit'
-          loading={loading}
-        >
-          جستجو
-        </Button>
-      </div>
-    </ReactHookFormWrapper>
+    </SearchFormWrapper>
   );
 }
 
